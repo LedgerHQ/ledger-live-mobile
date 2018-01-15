4e9f6d6a3a093bb263140b8d577b214c57258cc1
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-01-15 13:54:05 +0100

    sync

--- src/DebugHttpTransport.js
+++ src/DebugHttpTransport.js
@@ -3,7 +3,6 @@ import HttpTransport from "@ledgerhq/hw-transport-http";
 import Config from "react-native-config";
 
 const DEBUG_COMM_HTTP_PROXY = Config.DEBUG_COMM_HTTP_PROXY;
-//const DEBUG_COMM_HTTP_PROXY = null;
 
 export default class DebugHttpTransport extends HttpTransport {
   static list = (): * =>
