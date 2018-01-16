90be201e6726dfbb2e5458740062c0df7652e904
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-01-16 10:40:37 +0100

    improve DebugHttpTransport

--- src/components/RequestDevice/DebugHttpTransport.js
+++ src/components/RequestDevice/DebugHttpTransport.js
@@ -6,9 +6,42 @@ const DEBUG_COMM_HTTP_PROXY = Config.DEBUG_COMM_HTTP_PROXY;
 
 export default class DebugHttpTransport extends HttpTransport {
   static list = (): * =>
-    Promise.resolve(DEBUG_COMM_HTTP_PROXY ? [DEBUG_COMM_HTTP_PROXY] : []);
+    DEBUG_COMM_HTTP_PROXY
+      ? DebugHttpTransport.open(DEBUG_COMM_HTTP_PROXY).then(
+          () => [DEBUG_COMM_HTTP_PROXY],
+          () => []
+        )
+      : Promise.resolve([]);
+
   static discover = (observer: *) => {
-    if (DEBUG_COMM_HTTP_PROXY) observer.next(DEBUG_COMM_HTTP_PROXY);
-    return { unsubscribe: () => {} };
+    let unsubscribed = false;
+    if (DEBUG_COMM_HTTP_PROXY) {
+      function attemptToConnect() {
+        if (unsubscribed) return;
+        DebugHttpTransport.open(DEBUG_COMM_HTTP_PROXY, 5000).then(
+          () => {
+            if (unsubscribed) return;
+            observer.next(DEBUG_COMM_HTTP_PROXY);
+            observer.complete();
+          },
+          e => {
+            if (unsubscribed) return;
+            console.log(
+              "can't access " + DEBUG_COMM_HTTP_PROXY + " Retrying...",
+              e
+            );
+            setTimeout(attemptToConnect, 1000);
+          }
+        );
+      }
+      attemptToConnect();
+    } else {
+      observer.complete();
+    }
+    return {
+      unsubscribe: () => {
+        unsubscribed = true;
+      }
+    };
   };
 }
