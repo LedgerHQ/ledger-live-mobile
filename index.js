4c13dbf3ec1279c736ae322aafc5ac3359c8292f
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-01-17 14:05:27 +0100

    don't enable Sentry during dev

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import App from "./src";
 
 Sentry.config(
   "https://beb25fd89630498990fd16bbc5b92fc1:9ec602a751334c6087c74aef34042afd@sentry.io/273101"
-).install();
+);
+if (!__DEV__) {
+  Sentry.install();
+}
 
 AppRegistry.registerComponent("ledgerwalletmobile", () => App);
