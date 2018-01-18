6e3362b3fa69fd46f846678be521e2ff9654796f
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-01-18 15:28:09 +0100

    comment out the breaking sentry cmd

--- ios/ledgerwalletmobile/AppDelegate.m
+++ ios/ledgerwalletmobile/AppDelegate.m
@@ -28,9 +28,10 @@ RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"ledgerwalletmobile"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-
+#ifndef RCT_DEV
   [RNSentry installWithRootView:rootView];
-
+#endif
+  
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
