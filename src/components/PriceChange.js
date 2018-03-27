d031db21241f2059305d660a680b653a5528bd46
Thibaut Boustany <thibaut@ledger.fr>
2018-03-27 18:02:21 +0200

    PriceChange component for displaying percent change between two values with matching colors

---
+++ src/components/PriceChange.js
@@ -0,0 +1,34 @@
+// @flow
+import React, { Component } from "react";
+import LText from "./LText";
+import colors from "../colors";
+
+export default class PriceChange extends Component<{
+  before: number,
+  after: number,
+  color?: "western" | "eastern",
+  style?: {}
+}> {
+  render() {
+    const { before, after, color } = this.props;
+    const style: { color?: * } = {};
+
+    if (!before) {
+      return <LText />;
+    }
+
+    const percent: number = after / before * 100 - 100;
+
+    if (color === "western") {
+      style.color = percent >= 0 ? colors.green : colors.red;
+    } else if (color === "eastern") {
+      style.color = percent >= 0 ? colors.red : colors.blue;
+    }
+
+    return (
+      <LText style={[this.props.style, style]}>
+        {`${(percent >= 0 ? "+" : "") + percent.toFixed(2)} %`}
+      </LText>
+    );
+  }
+}
