e2c4ddf9f97b5a03369bdac6e502f4a5d4838ef1
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2018-10-17 13:36:23 +0200

    Add delay to genuine check

--- src/logic/hw/genuineCheck.js
+++ src/logic/hw/genuineCheck.js
@@ -1,9 +1,11 @@
 // @flow
 import Transport from "@ledgerhq/hw-transport";
+import { delay } from "../promise";
 
 // TODO will returns one of the specific error. errors are in live-common now
 
 export default async (transport: Transport<*>): Promise<void> => {
+  await delay(2000);
   // FIXME real genuine check!
   await transport.send(0, 0, 0, 0);
 };
