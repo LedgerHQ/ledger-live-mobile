88b91e8295aba1e2e44883ebac2e03ebb1278f8d
jsiguira <jun@dappface.com>
2020-03-11 15:34:37 +0100

    accept ScrollView props on NavigationScrollView

--- src/components/NavigationScrollView.js
+++ src/components/NavigationScrollView.js
@@ -4,17 +4,15 @@ import React, { useRef } from "react";
 import { ScrollView, StyleProp } from "react-native";
 import { useScrollToTop } from "../navigation/utils";
 
-interface Props {
-  children: any;
-  style: StyleProp<ScrollView>;
-}
-
-export default function NavigationScrollView({ children, style }: Props) {
+export default function NavigationScrollView({
+  children,
+  ...scrollViewProps
+}: any) {
   const ref = useRef();
   useScrollToTop(ref);
 
   return (
-    <ScrollView ref={ref} style={style}>
+    <ScrollView ref={ref} {...scrollViewProps}>
       {children}
     </ScrollView>
   );
