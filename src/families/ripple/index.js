c0a5f97f2355920b4595061d018f9811fcffe2b8
jsiguira <jun@dappface.com>
2020-03-17 16:38:47 +0100

    fix ripple edit fee flow

--- src/families/ripple/index.js
+++ src/families/ripple/index.js
@@ -1,5 +1,5 @@
 // @flow
-import * as ScreenEditFee from "./ScreenEditFee";
+import * as RippleEditFee from "./ScreenEditFee";
 import * as RippleEditTag from "./ScreenEditTag";
 
-export { ScreenEditFee, RippleEditTag };
+export { RippleEditFee, RippleEditTag };
