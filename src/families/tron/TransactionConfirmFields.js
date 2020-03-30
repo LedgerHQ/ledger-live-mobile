0091bdd09a2ac006e5f7a852ef2003c56c23423f
LukeSugiura <jun.sugiura.jp@gmail.com>
2020-03-30 15:51:54 +0200

    (Trx): added voting flow

--- src/families/tron/TransactionConfirmFields.js
+++ src/families/tron/TransactionConfirmFields.js
@@ -2,13 +2,18 @@
 import invariant from "invariant";
 import React from "react";
 import { StyleSheet } from "react-native";
+import { Trans } from "react-i18next";
 import type {
   AccountLike,
   Account,
   Transaction,
 } from "@ledgerhq/live-common/lib/types";
 import { getMainAccount } from "@ledgerhq/live-common/lib/account";
-import { Trans } from "react-i18next";
+import {
+  formatVotes,
+  useTronSuperRepresentatives,
+} from "@ledgerhq/live-common/lib/families/tron/react";
+
 import { DataRow } from "../../components/ValidateOnDeviceDataRow";
 import LText from "../../components/LText";
 import Info from "../../icons/Info";
@@ -31,6 +36,9 @@ const styles = StyleSheet.create({
     fontSize: 14,
     flex: 1,
     textAlign: "right",
+  },
+  greyText: {
+    color: colors.grey,
   },
 });
 
@@ -69,20 +77,59 @@ const Post = ({
   account: AccountLike,
   parentAccount: ?Account,
   transaction: Transaction,
+  t: *,
 }) => {
   const mainAccount = getMainAccount(account, parentAccount);
 
   invariant(transaction.family === "tron", "tron transaction");
 
+  const sp = useTronSuperRepresentatives();
+  const formattedVotes =
+    transaction.votes &&
+    transaction.votes.length &&
+    formatVotes(transaction.votes, sp);
+
+  const from =
+    account.type === "ChildAccount"
+      ? account.address
+      : mainAccount.freshAddress;
+
   return (
     <>
-      <DataRow label={<Trans i18nKey="ValidateOnDevice.address" />}>
+      {formattedVotes && formattedVotes.length > 0 ? (
+        <>
+          <DataRow>
+            <LText
+              style={[styles.text, styles.greyText, { textAlign: "left" }]}
+            >
+              <Trans i18nKey="ValidateOnDevice.name" />
+            </LText>
+            <LText style={[styles.text, styles.greyText]}>
+              <Trans i18nKey="ValidateOnDevice.amount" />
+            </LText>
+          </DataRow>
+          {formattedVotes.map(({ address, voteCount, validator }) => (
+            <DataRow key={address}>
+              <LText semiBold>{(validator && validator.name) || address}</LText>
+              <LText semiBold style={styles.text}>
+                {voteCount}
+              </LText>
+            </DataRow>
+          ))}
+        </>
+      ) : null}
+      <DataRow label={<Trans i18nKey="ValidateOnDevice.fromAddress" />}>
         <LText semiBold style={styles.text}>
-          {account.type === "ChildAccount"
-            ? account.address
-            : mainAccount.freshAddress}
+          {from}
         </LText>
       </DataRow>
+      {transaction.recipient && transaction.recipient !== from ? (
+        <DataRow label={<Trans i18nKey="ValidateOnDevice.toAddress" />}>
+          <LText semiBold style={styles.text}>
+            {transaction.recipient}
+          </LText>
+        </DataRow>
+      ) : null}
       {transaction.resource && (
         <DataRow label={<Trans i18nKey="ValidateOnDevice.resource" />}>
           <LText semiBold style={styles.text}>
---
+++ src/families/tron/VotingFlow/01-SelectValidator.js
@@ -0,0 +1,187 @@
+/* @flow */
+import invariant from "invariant";
+import useBridgeTransaction from "@ledgerhq/live-common/lib/bridge/useBridgeTransaction";
+import React, { useCallback } from "react";
+import { View, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-navigation";
+import { connect } from "react-redux";
+import { Trans } from "react-i18next";
+import i18next from "i18next";
+
+import type { NavigationScreenProp } from "react-navigation";
+import type { Account, Transaction } from "@ledgerhq/live-common/lib/types";
+import { getAccountBridge } from "@ledgerhq/live-common/lib/bridge";
+
+import { accountAndParentScreenSelector } from "../../../reducers/accounts";
+import colors from "../../../colors";
+import { TrackScreen } from "../../../analytics";
+import Button from "../../../components/Button";
+import StepHeader from "../../../components/StepHeader";
+import RetryButton from "../../../components/RetryButton";
+import CancelButton from "../../../components/CancelButton";
+import GenericErrorBottomModal from "../../../components/GenericErrorBottomModal";
+
+const forceInset = { bottom: "always" };
+
+type Props = {
+  account: Account,
+  navigation: NavigationScreenProp<{
+    params: {
+      accountId: string,
+      transaction: Transaction,
+    },
+  }>,
+};
+
+const SelectValidator = ({ account, navigation }: Props) => {
+  invariant(
+    account && account.tronResources,
+    "account and tron resources required",
+  );
+  const bridge = getAccountBridge(account, undefined);
+
+  const {
+    transaction,
+    status,
+    setTransaction,
+    bridgeError,
+    bridgePending,
+  } = useBridgeTransaction(() => {
+    const t = bridge.createTransaction(account);
+
+    const { tronResources } = account;
+    const { votes } = tronResources;
+
+    const transaction = bridge.updateTransaction(t, {
+      mode: "vote",
+      votes,
+    });
+
+    return { account, transaction };
+  });
+
+  // const onChange = useCallback(
+  //   (address, vote) => {
+  //     /** @TODO update vote transaction */
+  //     // setTransaction(
+  //     //   bridge.updateTransaction(transaction, {
+  //     //     amount: getDecimalPart(amount, defaultUnit.magnitude),
+  //     //   }),
+  //     // );
+  //   },
+  //   [setTransaction, transaction, bridge, defaultUnit],
+  // );
+
+  const onContinue = useCallback(() => {
+    navigation.navigate("CastVote", {
+      accountId: account.id,
+      transaction,
+      status,
+    });
+  }, [account, navigation, transaction, status]);
+
+  const onBridgeErrorCancel = useCallback(() => {
+    const parent = navigation.dangerouslyGetParent();
+    if (parent) parent.goBack();
+  }, [navigation]);
+
+  const onBridgeErrorRetry = useCallback(() => {
+    if (!transaction) return;
+    setTransaction(bridge.updateTransaction(transaction, {}));
+  }, [setTransaction, transaction, bridge]);
+
+  if (!account || !transaction) return null;
+
+  // const error = bridgePending ? null : status.errors.votes;
+  // const warning = status.warnings.votes;
+
+  return (
+    <>
+      <TrackScreen category="Vote" name="SelectValidator" />
+      <SafeAreaView style={styles.root} forceInset={forceInset}>
+        <View style={styles.root} />
+        <View style={styles.bottomWrapper}>
+          <View style={styles.continueWrapper}>
+            <Button
+              event="SelectValidatorContinue"
+              type="primary"
+              title={
+                <Trans
+                  i18nKey={
+                    !bridgePending
+                      ? "common.continue"
+                      : "vote.amount.loadingNetwork"
+                  }
+                />
+              }
+              onPress={onContinue}
+              disabled={!!status.errors.amount || bridgePending}
+              pending={bridgePending}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+
+      <GenericErrorBottomModal
+        error={bridgeError}
+        onClose={onBridgeErrorRetry}
+        footerButtons={
+          <>
+            <CancelButton
+              containerStyle={styles.button}
+              onPress={onBridgeErrorCancel}
+            />
+            <RetryButton
+              containerStyle={[styles.button, styles.buttonRight]}
+              onPress={onBridgeErrorRetry}
+            />
+          </>
+        }
+      />
+    </>
+  );
+};
+
+SelectValidator.navigationOptions = {
+  headerTitle: (
+    <StepHeader
+      title={i18next.t("vote.stepperHeader.selectValidator")}
+      subtitle={i18next.t("vote.stepperHeader.stepRange", {
+        currentStep: "1",
+        totalSteps: "4",
+      })}
+    />
+  ),
+  headerLeft: null,
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: colors.lightGrey,
+  },
+  topContainer: { paddingHorizontal: 32 },
+  bottomWrapper: {
+    alignSelf: "stretch",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: 16,
+    backgroundColor: colors.white,
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderTopColor: colors.lightGrey,
+  },
+  button: {
+    flex: 1,
+    marginHorizontal: 8,
+  },
+  buttonRight: {
+    marginLeft: 8,
+  },
+  continueWrapper: {
+    alignSelf: "stretch",
+    alignItems: "stretch",
+    justifyContent: "flex-end",
+  },
+});
+
+export default connect(accountAndParentScreenSelector)(SelectValidator);
