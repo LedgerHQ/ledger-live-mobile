0284126595ffdcfca16eaf55e5e5fed23d2d4848
LukeSugiura <jun.sugiura.jp@gmail.com>
2020-04-01 17:32:24 +0200

    markup SR list

--- src/families/tron/VotingFlow/01-SelectValidator/Main.js
+++ src/families/tron/VotingFlow/01-SelectValidator/Main.js
@@ -1,9 +1,10 @@
 // @flow
+import { BigNumber } from "bignumber.js";
 import invariant from "invariant";
-import React, { useMemo } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { translate } from "react-i18next";
 import type { TFunction } from "react-i18next";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
 import { SectionList } from "react-navigation";
 import {
   useTronSuperRepresentatives,
@@ -11,7 +12,9 @@ import {
 } from "@ledgerhq/live-common/lib/families/tron/react";
 import type { Transaction } from "@ledgerhq/live-common/lib/types";
 import colors from "../../../../colors";
+import CheckBox from "../../../../components/CheckBox";
 import LText from "../../../../components/LText";
+import Trophy from "../../../../icons/Trophy";
 
 type Props = {
   transaction: Transaction,
@@ -42,21 +45,59 @@ function SelectValidatorMain({ transaction, t }: Props) {
     [sortedSuperRepresentatives],
   );
 
+  const [selected, setSelected] = useState<{
+    [address: string]: true,
+  }>({});
+
+  console.log(JSON.stringify(sortedSuperRepresentatives[0], null, 2));
+
+  const onPressItemCreator = useCallback(
+    ({ address }) => () => {
+      const isSelected = !!selected[address];
+      setSelected({ ...selected, [address]: isSelected ? undefined : true });
+    },
+    [selected],
+  );
+
   return (
     <SectionList
       sections={sections}
       keyExtractor={({ address }, i) => address + i}
-      renderItem={({ item }) => (
+      renderItem={({ item, index }) => {
+        const { sr } = item;
+
+        return (
+          <TouchableOpacity onPress={onPressItemCreator(item)}>
+            <View>
+              <Trophy size={16} color={colors.darkBlue} />
+            </View>
+
+            <View>
+              <LText>
+                {index + 1}. {sr.name}
+              </LText>
+              <LText>
+                {BigNumber(sr.voteCount).toFormat()} ({sr.brokerage}%)
+              </LText>
+            </View>
+
+            <View>
+              <LText>{"6,8"} %</LText>
+              <LText>Est. yield</LText>
+            </View>
+
+            <View>
+              <CheckBox isChecked={!!selected[sr.address]} />
+            </View>
+          </TouchableOpacity>
+        );
+      }}
+      renderSectionHeader={({ section: { type } }) => (
         <View>
-          <LText style={{ color: colors.darkBlue }}>
-            {JSON.stringify(item, null, 2)}
+          <LText>
+            {t(`tron.voting.flow.selectValidator.sections.title.${type}`)}
           </LText>
         </View>
-      )}
-      renderSectionHeader={({ section: { type } }) => (
-        <LText>
-          {t(`tron.voting.flow.selectValidator.sections.title.${type}`)}
-        </LText>
       )}
     />
   );
