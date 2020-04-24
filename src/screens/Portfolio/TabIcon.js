a53156d8b37a582d2eebae5a0e47824192071356
LukeSugiura <jun.sugiura.jp@gmail.com>
2020-04-24 13:21:50 +0200

    prevent crashing from tapping portfolio tab when there is no account

--- src/screens/Portfolio/TabIcon.js
+++ src/screens/Portfolio/TabIcon.js
@@ -1,15 +1,18 @@
 // @flow
 import React from "react";
 import { TouchableOpacity } from "react-native";
+import { useSelector } from "react-redux";
 import { useIsFocused } from "@react-navigation/native";
 import TabIcon from "../../components/TabIcon";
 import PortfolioIcon from "../../icons/Portfolio";
 import { scrollToTop } from "../../navigation/utils";
+import { accountsSelector } from "../../reducers/accounts";
 
 export default function PortfolioTabIcon(props: Props) {
+  const accounts = useSelector(accountsSelector);
   const isFocused = useIsFocused();
 
-  if (!isFocused) {
+  if (!isFocused || accounts.length === 0) {
     return <TabIcon Icon={PortfolioIcon} i18nKey="tabs.portfolio" {...props} />;
   }
 
