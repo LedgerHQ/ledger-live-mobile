320cdf49d2ff2025a1f617d7a3ca5b2d3924c143
JunichiSugiura <jun.sugiura.jp@gmail.com>
2020-05-07 14:42:30 +0200

    Setup detox for iOS

---
+++ e2e/firstTest.spec.js
@@ -0,0 +1,9 @@
+describe("Ledger Live Mobile", () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it("should have welcome screen", async () => {
+    await element(by.id("OnboardingWelcomeContinue")).tap();
+  });
+});
