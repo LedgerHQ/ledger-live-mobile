20bfb410992f119a5e444722b198e36d26de71f8
JunichiSugiura <jun.sugiura.jp@gmail.com>
2020-05-27 10:40:14 +0200

    add close button

--- src/families/cosmos/Delegations/Drawer.js
+++ src/families/cosmos/Delegations/Drawer.js
@@ -1,7 +1,9 @@
 // @flow
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
 import BottomModal from "../../../components/BottomModal";
+import Close from "../../../icons/Close";
+import colors from "../../../colors";
 
 type Props = {
   isOpen: boolean,
@@ -16,12 +18,28 @@ export default function DelegationDrawer({ isOpen, onClose }: Props) {
       isOpened={isOpen}
       onClose={onClose}
     >
-      <View />
+      <View>
+        <TouchableOpacity style={styles.closeIconWrapper} onPress={onClose}>
+          <Close />
+        </TouchableOpacity>
+      </View>
     </BottomModal>
   );
 }
+
+const CLOSE_ICON_WRAPPER_SIZE = 32;
+
 const styles = StyleSheet.create({
   root: {
     padding: 16,
+  },
+  closeIconWrapper: {
+    width: CLOSE_ICON_WRAPPER_SIZE,
+    height: CLOSE_ICON_WRAPPER_SIZE,
+    borderRadius: CLOSE_ICON_WRAPPER_SIZE / 2,
+    backgroundColor: colors.lightFog,
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "flex-end",
   },
 });
