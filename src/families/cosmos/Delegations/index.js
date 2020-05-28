f3566ec15249fea9b2543b0563e04a1c4ea66dc3
JunichiSugiura <jun.sugiura.jp@gmail.com>
2020-05-28 11:17:15 +0200

    markup drawer

--- src/families/cosmos/Delegations/index.js
+++ src/families/cosmos/Delegations/index.js
@@ -1,24 +1,40 @@
 // @flow
-import React, { useCallback, useState } from "react";
+import { BigNumber } from "bignumber.js";
+import React, { useCallback, useState, useMemo } from "react";
+import type { ElementProps } from "react";
 import { View, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useTranslation } from "react-i18next";
 import { getAccountCurrency } from "@ledgerhq/live-common/lib/account";
 import { useCosmosMappedDelegations } from "@ledgerhq/live-common/lib/families/cosmos/react";
+import type { CosmosMappedDelegation } from "@ledgerhq/live-common/lib/families/cosmos/types";
 import type { Account } from "@ledgerhq/live-common/lib/types";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import DelegationInfo from "../../../components/DelegationInfo";
 import IlluRewards from "../../../components/IlluRewards";
 import { urls } from "../../../config/urls";
 import AccountSectionLabel from "../../../components/AccountSectionLabel";
-import colors from "../../../colors";
+import DelegationDrawer, {
+  styles as drawerStyles,
+} from "../../../components/DelegationDrawer";
+import type { IconProps } from "../../../components/DelegationDrawer";
+import colors, { rgba } from "../../../colors";
 import { ScreenName, NavigatorName } from "../../../const";
 import DelegationRow from "./Row";
-import DelegationDrawer from "./Drawer";
 import DelegationLabelRight from "./LabelRight";
+import Circle from "../../../components/Circle";
+import LText from "../../../components/LText";
+import DelegateIcon from "../../../icons/Delegate";
+import RedelegateIcon from "../../../icons/Redelegate";
+import UndelegateIcon from "../../../icons/Undelegate";
+import ClaimRewardIcon from "../../../icons/ClaimReward";
 
 type Props = {
   account: Account,
 };
+
+type DelegationDrawerProps = ElementProps<typeof DelegationDrawer>;
+type DelegationDrawerActions = $PropertyType<DelegationDrawerProps, "actions">;
 
 export default function Deleagations({ account }: Props) {
   const { t } = useTranslation();
@@ -26,7 +42,57 @@ export default function Deleagations({ account }: Props) {
   const currency = getAccountCurrency(account);
   const navigation = useNavigation();
 
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [delegation, setDelegation] = useState<?CosmosMappedDelegation>();
+
+  const data = useMemo<$PropertyType<DelegationDrawerProps, "data">>(
+    () =>
+      delegation
+        ? [
+            {
+              label: t("delegation.validator"),
+              Component:
+                delegation.validator?.name ?? delegation.validatorAddress ?? "",
+            },
+            {
+              label: t("delegation.validatorAddress"),
+              Component: () => {
+                return (
+                  // TODO link to explorer
+                  <TouchableOpacity onPress={() => {}}>
+                    <LText
+                      numberOfLines={1}
+                      semiBold
+                      ellipsizeMode="middle"
+                      style={[
+                        drawerStyles.valueText,
+                        drawerStyles.valueTextTouchable,
+                      ]}
+                    >
+                      {delegation.validatorAddress}
+                    </LText>
+                  </TouchableOpacity>
+                );
+              },
+            },
+            {
+              label: t("delegation.delegatedAccount"),
+              Component: account.name,
+            },
+            {
+              label: t("cosmos.delegation.drawer.status"),
+              Component:
+                delegation.status === "bonded"
+                  ? t("cosmos.delegation.drawer.active")
+                  : t("cosmos.delegation.drawer.inactive"),
+            },
+            {
+              label: t("cosmos.delegation.drawer.rewards"),
+              Component: delegation.formattedAmount ?? "",
+            },
+          ]
+        : [],
+    [delegation, t, account],
+  );
 
   const onDelegate = useCallback(() => {
     // TODO: check destination and params.
@@ -39,17 +105,92 @@ export default function Deleagations({ account }: Props) {
     });
   }, [navigation, account]);
 
-  const onSeeMore = useCallback(() => {
-    setIsDrawerOpen(true);
+  const onRedelegate = useCallback(() => {
+    // TODO: check destination and params.
+    navigation.navigate(NavigatorName.CosmosRedelegationFlow);
+  }, [navigation]);
+
+  const onCollectRewards = useCallback(() => {
+    // TODO: check destination and params.
+    navigation.navigate(NavigatorName.CosmosCollectRewardsFlow);
+  }, [navigation]);
+
+  const onUndelegate = useCallback(() => {
+    // TODO: check destination and params.
+    navigation.navigate(NavigatorName.CosmosUndelegationFlow);
+  }, [navigation]);
+
+  const onSeeMore = useCallback((delegation: CosmosMappedDelegation) => {
+    setDelegation(delegation);
   }, []);
 
   const onCloseDrawer = useCallback(() => {
-    setIsDrawerOpen(false);
+    setDelegation();
   }, []);
+
+  const actions = useMemo<DelegationDrawerActions>(
+    () => [
+      {
+        label: t("delegation.actions.delegate"),
+        Icon: (props: IconProps) => (
+          <Circle {...props} bg={colors.fog}>
+            <DelegateIcon />
+          </Circle>
+        ),
+        onPress: onDelegate,
+        event: "DelegationActionDelegate",
+      },
+      {
+        label: t("delegation.actions.redelegate"),
+        Icon: (props: IconProps) => (
+          <Circle {...props} bg={colors.fog}>
+            <RedelegateIcon />
+          </Circle>
+        ),
+        onPress: onRedelegate,
+        event: "DelegationActionRedelegate",
+      },
+      {
+        label: t("delegation.actions.collectRewards"),
+        Icon: (props: IconProps) => (
+          <Circle {...props} bg={rgba(colors.yellow, 0.2)}>
+            <ClaimRewardIcon />
+          </Circle>
+        ),
+        onPress: onCollectRewards,
+        event: "DelegationActionCollectRewards",
+      },
+      {
+        label: t("delegation.actions.undelegate"),
+        Icon: (props: IconProps) => (
+          <Circle {...props} bg={rgba(colors.alert, 0.2)}>
+            <UndelegateIcon />
+          </Circle>
+        ),
+        onPress: onUndelegate,
+        event: "DelegationActionUndelegate",
+      },
+    ],
+    [t, onDelegate, onRedelegate, onCollectRewards, onUndelegate],
+  );
 
   return (
     <View style={styles.root}>
-      <DelegationDrawer isOpen={isDrawerOpen} onClose={onCloseDrawer} />
+      <DelegationDrawer
+        isOpen={!!delegation}
+        onClose={onCloseDrawer}
+        account={account}
+        ValidatorImage={({ size }) => (
+          <Circle crop size={size}>
+            {/* TODO use FirstLetterIcon */}
+            <LText style={{ color: colors.white }}>TODO</LText>
+          </Circle>
+        )}
+        // TODO Check CosmosMappedDelegation type
+        amount={delegation?.amount ?? BigNumber(0)}
+        data={data}
+        actions={actions}
+      />
 
       {delegations.length === 0 ? (
         <DelegationInfo
@@ -74,7 +215,7 @@ export default function Deleagations({ account }: Props) {
               <DelegationRow
                 delegation={d}
                 currency={currency}
-                onPress={onSeeMore}
+                onPress={() => onSeeMore(d)}
                 isLast={i === delegations.length - 1}
               />
             </View>
