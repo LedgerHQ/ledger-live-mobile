9094ffd945913eacc93ca8c9b11a7b12c6d12f4f
Thibaut Boustany <thibaut@kastor.fr>
2020-08-31 18:31:53 +0200

    Export swap operations

--- src/screens/Swap/History/index.js
+++ src/screens/Swap/History/index.js
@@ -7,18 +7,25 @@ import {
   RefreshControl,
 } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
+import { useTranslation } from "react-i18next";
+import Share from "react-native-share";
 import getCompleteSwapHistory from "@ledgerhq/live-common/lib/swap/getCompleteSwapHistory";
 import updateAccountSwapStatus from "@ledgerhq/live-common/lib/swap/updateAccountSwapStatus";
+import { mappedSwapOperationsToCSV } from "@ledgerhq/live-common/lib/swap/csvExport";
 import { updateAccountWithUpdater } from "../../../actions/accounts";
 import { flattenAccountsSelector } from "../../../reducers/accounts";
 import OperationRow from "./OperationRow";
 import EmptyState from "./EmptyState";
 import LText from "../../../components/LText";
+import Button from "../../../components/Button";
 import colors from "../../../colors";
+import logger from "../../../logger";
+import DownloadFileIcon from "../../../icons/DownloadFile";
 
 const AnimatedSectionList = Animated.createAnimatedComponent(SectionList);
 
 const History = () => {
+  const { t } = useTranslation();
   const accounts = useSelector(flattenAccountsSelector);
   const dispatch = useDispatch();
   const [sections, setSections] = useState([]);
@@ -66,6 +73,30 @@ const History = () => {
     <OperationRow item={item} />
   );
 
+  const exportSwapHistory = async () => {
+    const history = await getCompleteSwapHistory(accountsRef.current);
+    const mapped = await mappedSwapOperationsToCSV(history);
+    const base64 = Buffer.from(mapped).toString("base64");
+    const options = {
+      title: t("transfer.swap.history.exportButton"),
+      message: t("transfer.swap.history.exportButton"),
+      failOnCancel: false,
+      saveToFiles: true,
+      type: "text/csv",
+      filename: t("transfer.swap.history.exportFilename"),
+      url: `data:text/csv;base64,${base64}`,
+    };
+
+    try {
+      await Share.open(options);
+    } catch (err) {
+      // `failOnCancel: false` is not enough to prevent throwing on cancel apparently ¯\_(ツ)_/¯
+      if (err.error.code !== "ECANCELLED500") {
+        logger.critical(err);
+      }
+    }
+  };
+
   return (
     <View style={styles.root}>
       <AnimatedSectionList
@@ -81,6 +112,17 @@ const History = () => {
             refreshing={isRefreshing}
             onRefresh={() => setIsRefreshing(true)}
           />
+        }
+        ListHeaderComponent={
+          sections.length && (
+            <Button
+              type="tertiary"
+              title={t("transfer.swap.history.exportButton")}
+              containerStyle={styles.button}
+              IconLeft={DownloadFileIcon}
+              onPress={exportSwapHistory}
+            />
+          )
         }
         keyExtractor={({ swapId }) => swapId}
         renderItem={renderItem}
@@ -119,6 +161,9 @@ const styles = StyleSheet.create({
   contentContainer: {
     paddingBottom: 64,
     flexGrow: 1,
+  },
+  button: {
+    margin: 16,
   },
 });
 
