9c43a22decd10ecc46519d27de1e69a63b2f2a52
JunichiSugiura <jun.sugiura.jp@gmail.com>
2020-09-07 19:14:21 +0200

    fix bug in tx success

--- src/logic/screenTransactionHooks.js
+++ src/logic/screenTransactionHooks.js
@@ -198,7 +198,7 @@ export function useSignedTxHandler({
         navigation.replace(
           route.name.replace("ConnectDevice", "ValidationSuccess"),
           {
-            accountId: route.params.accountId,
+            ...route.params,
             result: operation,
           },
         );
