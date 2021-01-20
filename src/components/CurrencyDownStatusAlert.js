f219e2c6ec3e1a686e664371595612297fd002d3
LFBarreto <lfb.access@gmail.com>
2021-01-20 12:00:17 +0100

    (Stratis): add eol warning banner

--- src/components/CurrencyDownStatusAlert.js
+++ src/components/CurrencyDownStatusAlert.js
@@ -1,13 +1,22 @@
 // @flow
+import React from "react";
+import { createCustomErrorClass } from "@ledgerhq/errors";
 import type {
   TokenCurrency,
   CryptoCurrency,
 } from "@ledgerhq/live-common/lib/types";
+import WarningBanner from "./WarningBanner";
 
 type Props = {
   currencies: Array<CryptoCurrency | TokenCurrency>,
 };
+const StratisDown2021Warning = createCustomErrorClass("StratisDown2021Warning");
 
-const CurrencyDownStatusAlert = (_: Props) => null;
+const CurrencyDownStatusAlert = ({ currencies }: Props) => {
+  if (currencies.some(c => c.id === "stratis")) {
+    return <WarningBanner error={new StratisDown2021Warning()} />;
+  }
+  return null;
+};
 
 export default CurrencyDownStatusAlert;
--- src/config/urls.js
+++ src/config/urls.js
@@ -28,6 +28,8 @@ export const urls = {
       "https://support.ledger.com/hc/en-us/articles/360012109220?utm_source=ledger_live_mobile&utm_medium=self_referral&utm_content=error_syncerror",
     LedgerAPIErrorWithMessage:
       "https://status.ledger.com?utm_source=ledger_live_mobile&utm_medium=self_referral&utm_content=error_apierrorwithmessage",
+    StratisDown2021Warning:
+      "https://support.ledger.com/hc/en-us/articles/115005175329-Stratis-STRAT-",
   },
   multipleAddresses:
     "https://support.ledger.com/hc/en-us/articles/360033802154?utm_source=ledger_live_mobile&utm_medium=self_referral&utm_content=ops_details_change",
