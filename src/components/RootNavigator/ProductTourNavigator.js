24e46843890c167b999d2711ac00611c2db70634
machard <matthieu__@hotmail.fr>
2021-02-23 15:32:17 +0100

    maybe later link for buy

--- src/components/RootNavigator/ProductTourNavigator.js
+++ src/components/RootNavigator/ProductTourNavigator.js
@@ -1,19 +1,25 @@
 // @flow
-import React, { useMemo } from "react";
+import React, { useMemo, useContext } from "react";
 import { Platform } from "react-native";
 import {
   createStackNavigator,
   HeaderBackButton,
 } from "@react-navigation/stack";
-import { useTheme } from "@react-navigation/native";
+import { useTranslation } from "react-i18next";
+import { useTheme, useNavigation } from "@react-navigation/native";
 import { ScreenName, NavigatorName } from "../../const";
 import ProductTourMenu from "../../screens/ProductTour/ProductTourMenu";
+import { context, completeStep } from "../../screens/ProductTour/Provider";
+import Button from "../Button";
 import ProductTourStepStart from "../../screens/ProductTour/ProductTourStepStart";
 import { getStackNavigatorConfig } from "../../navigation/navigatorConfig";
 import { navigate } from "../../rootnavigation";
 
 export default function ProductTourNavigator() {
   const { colors } = useTheme();
+  const ptContext = useContext(context);
+  const { t } = useTranslation();
+  const navigation = useNavigation();
   const stackNavigationConfig = useMemo(
     () => getStackNavigatorConfig(colors, true),
     [colors],
@@ -61,7 +67,22 @@ export default function ProductTourNavigator() {
         component={ProductTourStepStart}
         options={{
           headerShown: true,
-          headerRight: null,
+          headerRight:
+            ptContext.currentStep === "BUY_COINS"
+              ? () => (
+                  <Button
+                    type="primary"
+                    containerStyle={{ backgroundColor: "transparent" }}
+                    onPress={() =>{
+                      completeStep(ptContext.currentStep);
+                      navigation.navigate(NavigatorName.ProductTour, {
+                        screen: ScreenName.ProductTourMenu,
+                      });
+                    }}
+                    title={t("producttour.stepstart.buymaybe")}
+                  />
+                )
+              : null,
           headerTitle: null,
           headerStyle: {
             ...headerStyle,
