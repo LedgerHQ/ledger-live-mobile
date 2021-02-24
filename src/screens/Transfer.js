4f03350938bb382ec68b17ffbe21bb8bd83f9b91
machard <matthieu__@hotmail.fr>
2021-02-24 10:40:37 +0100

    disable overlay when click sur tranfer icon before openibng modal

--- src/screens/Transfer.js
+++ src/screens/Transfer.js
@@ -1,10 +1,11 @@
 /* @flow */
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import Touchable from "../components/Touchable";
 import TabIcon from "../components/TabIcon";
 import CreateModal from "../modals/Create";
 import TransferIcon from "../icons/Transfer";
 import { lockSubject } from "../components/RootNavigator/CustomBlockRouterNavigator";
+import { enableHole, context } from "./ProductTour/Provider";
 
 const hitSlop = {
   top: 10,
@@ -22,8 +23,12 @@ export default () => null;
 
 export function TransferTabIcon(props: Props) {
   const [isModalOpened, setIsModalOpened] = useState(false);
+  const ptContext = useContext(context);
 
   function openModal() {
+    if (ptContext.holeConfig) {
+      enableHole(`-${ptContext.holeConfig}`);
+    }
     setIsModalOpened(true);
   }
 
