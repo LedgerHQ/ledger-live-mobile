d9203019ced2b8cde56ebcc8c2e5593e001b1e73
Juan Cortes Ross <juan@bohem.io>
2021-03-02 13:07:10 +0100

    LL-4537 input clear button for dark/dusk themes

--- src/components/TextInput.ios.js
+++ src/components/TextInput.ios.js
@@ -1,47 +1,74 @@
 /* @flow */
 
-import { TextInput as ReactNativeTextInput } from "react-native";
-import React, { PureComponent } from "react";
+import React, { useState } from "react";
 import { useTheme } from "@react-navigation/native";
+import {
+  View,
+  TextInput as ReactNativeTextInput,
+  StyleSheet,
+} from "react-native";
+import Icon from "react-native-vector-icons/dist/Ionicons";
+import Touchable from "./Touchable";
 
-class TextInput extends PureComponent<*> {
-  render() {
-    const {
-      containerStyle, // Needed to pass flow, since we call the native TextInput
-      withSuggestions,
-      innerRef,
-      style,
-      colors,
-      ...otherProps
-    } = this.props;
+const TextInput = ({
+  containerStyle, // Needed to pass flow, since we call the native TextInput
+  withSuggestions,
+  innerRef,
+  style,
+  onInputCleared,
+  clearButtonMode,
+  value,
+  ...otherProps
+}: *) => {
+  const { colors, dark } = useTheme();
+  const [focused, setFocused] = useState(false);
 
-    const flags = {};
+  const flags = {};
+  if (!withSuggestions) {
+    flags.autoCorrect = false;
+  }
 
-    if (!withSuggestions) {
-      flags.autoCorrect = false;
-    }
+  // In order to show the correct close icon on the input we need a custom impl.
+  const shouldShowClearButton =
+    !!value &&
+    ((focused && clearButtonMode === "while-editing") ||
+      clearButtonMode === "always");
 
-    return (
+  return (
+    <View style={[styles.container, containerStyle]}>
       <ReactNativeTextInput
         ref={innerRef}
+        keyboardAppearance={dark ? "dark" : "light"}
         allowFontScaling={false}
+        onBlur={() => setFocused(false)}
+        onFocus={() => setFocused(true)}
+        value={value}
         {...otherProps}
         {...flags}
         style={[style, { color: colors.darkBlue }]}
       />
-    );
-  }
-}
-
-// $FlowFixMe https://github.com/facebook/flow/pull/5920
-export default React.forwardRef((props, ref) => {
-  const { colors, dark } = useTheme();
-  return (
-    <TextInput
-      innerRef={ref}
-      colors={colors}
-      keyboardAppearance={dark ? "dark" : "light"}
-      {...props}
-    />
+      {shouldShowClearButton ? (
+        <Touchable
+          style={styles.clearWrapper}
+          event="TextInputClearValue"
+          onPress={onInputCleared}
+        >
+          <Icon name="ios-close-circle" color={colors.grey} size={18} />
+        </Touchable>
+      ) : null}
+    </View>
   );
+};
+
+export default TextInput;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+  },
+  clearWrapper: {
+    paddingHorizontal: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
