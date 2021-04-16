2f4132b06f673e138db415c1f3523d4304884371
huiqi <huiqi.zheng@ledger.fr>
2021-04-16 15:23:03 +0200

    fix lint error

--- src/components/TranslatedError.js
+++ src/components/TranslatedError.js
@@ -23,15 +23,20 @@ export default function TranslatedError({ error, field = "title" }: Props) {
   if (error.name) {
     let translation = t(`errors.${error.name}.${field}`, arg);
     if (translation !== `errors.${error.name}.${field}`) {
-      if ((typeof translation === "object") && ("productName" in arg)) { // it has specific translation for different device and platform
+      if (typeof translation === "object" && "productName" in arg) {
+        // it has specific translation for different device and platform
         const platform = Platform.OS;
-        const device = arg.productName.includes("Nano S")? "nanoS" : "nanoX";
-        translation = t(`errors.${error.name}.${field}.${platform}.${device}`, arg);
-        if (translation !== `errors.${error.name}.${field}.${platform}.${device}`) {
+        const device = arg.productName.includes("Nano S") ? "nanoS" : "nanoX";
+        translation = t(
+          `errors.${error.name}.${field}.${platform}.${device}`,
+          arg,
+        );
+        if (
+          translation !== `errors.${error.name}.${field}.${platform}.${device}`
+        ) {
           return translation;
         }
-      }
-      else if (typeof translation === "string") {
+      } else if (typeof translation === "string") {
         return translation;
       }
     }
