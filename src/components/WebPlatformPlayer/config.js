c8db91abdbd7ed4e279bf89674d53f83cb09b750
haammar-ledger <hakim.aammar-ext@ledger.fr>
2021-04-29 18:15:06 +0200

    WIP PoC - can send and receive jsonrcp messages with the DAPP Browser

---
+++ src/components/WebPlatformPlayer/config.js
@@ -0,0 +1,54 @@
+// @flow
+import qs from "qs";
+
+const DAPP_BROWSER =
+  "https://iframe-dapp-browser-test.vercel.app/app/dapp-browser";
+
+export const PlatformsConfig = {
+  debug: {
+    isDapp: false,
+    url: "https://iframe-dapp-browser-test.vercel.app/app/debug",
+    name: "Debugger",
+    icon: null,
+  },
+  paraswap: {
+    isDapp: true,
+    url: DAPP_BROWSER,
+    name: "ParaSwap",
+    params: {
+      url: "https://paraswap-ui-ledger.herokuapp.com/?embed=true",
+      appName: "paraswap",
+    },
+    // $FlowFixMe
+    icon: require("../../images/platform/paraswap.png").default,
+  },
+};
+
+export const getPlatformUrl = (platform: string, t?: Number) => {
+  const config = PlatformsConfig[platform];
+  if (!config) {
+    return null;
+  }
+
+  const url = config.isDapp ? DAPP_BROWSER : config.url;
+
+  const params = {
+    t,
+    ...config.params,
+  };
+
+  return `${url}?${qs.stringify(params)}`;
+};
+
+export const getPlatformOrigin = (platform: string) => {
+  const config = PlatformsConfig[platform];
+  if (!config) {
+    return null;
+  }
+
+  return new URL(config.url).origin;
+};
+
+export const getPlatformName = (platform: string) => {
+  return PlatformsConfig[platform]?.name || null;
+};
--- src/screens/Swap/FormOrHistory.js
+++ src/screens/Swap/FormOrHistory.js
@@ -8,10 +8,13 @@ import type {
 } from "@ledgerhq/live-common/lib/types/account";
 import { useTheme } from "@react-navigation/native";
 import { ScreenName } from "../../const";
+import Platform from "../../components/WebPlatformPlayer";
 import Swap from "./Swap";
 import History from "./History";
 import styles from "../../navigation/styles";
 import LText from "../../components/LText";
+
+// TODO: Rename this file/screen
 
 type RouteParams = {
   defaultAccount: ?AccountLike,
@@ -26,6 +29,10 @@ type TabLabelProps = {
 export default ({ route }: { route: { params: RouteParams } }) => {
   const { t } = useTranslation();
   const { colors } = useTheme();
+
+  // FIXME: Hardcoded for now
+  const platform = "paraswap";
+
   return (
     <Tab.Navigator
       tabBarOptions={{
@@ -35,6 +42,21 @@ export default ({ route }: { route: { params: RouteParams } }) => {
         },
       }}
     >
+      <Tab.Screen
+        name={ScreenName.SwapPlatform}
+        options={{
+          title: t("exchange.buy.tabTitle"),
+          tabBarLabel: ({ focused, color }: TabLabelProps) => (
+            <LText style={{ width: "110%", color }} semiBold={focused}>
+              {t("transfer.swap.platform.tab")}
+            </LText>
+          ),
+        }}
+      >
+        {props => (
+          <Platform {...props} {...route?.params} platform={platform} />
+        )}
+      </Tab.Screen>
       <Tab.Screen
         name={ScreenName.SwapForm}
         options={{
