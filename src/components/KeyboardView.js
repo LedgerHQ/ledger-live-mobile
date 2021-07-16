a6500e661a785cd6fb0c47f0acbe4056bca690f2
jelbaz-ledger <julien.el-baz@ledger.fr>
2021-07-16 09:55:57 +0200

    style: use named import

--- src/components/KeyboardView.js
+++ src/components/KeyboardView.js
@@ -1,7 +1,7 @@
 // @flow
 import React from "react";
 import { KeyboardAvoidingView, Platform, NativeModules } from "react-native";
-import * as ExperimentalHeader from "../screens/Settings/Experimental/ExperimentalHeader";
+import { HEIGHT as ExperimentalHeaderHeight } from "../screens/Settings/Experimental/ExperimentalHeader";
 import useExperimental from "../screens/Settings/Experimental/useExperimental";
 
 const { DeviceInfo } = NativeModules;
@@ -15,7 +15,7 @@ const KeyboardView = React.memo<Props>(
   ({ style = { flex: 1 }, children }: *) => {
     const isExperimental = useExperimental();
     let behavior;
-    let keyboardVerticalOffset = isExperimental ? ExperimentalHeader.HEIGHT : 0;
+    let keyboardVerticalOffset = isExperimental ? ExperimentalHeaderHeight : 0;
     if (Platform.OS === "ios") {
       keyboardVerticalOffset += DeviceInfo.isIPhoneX_deprecated ? 88 : 64;
       behavior = "padding";
