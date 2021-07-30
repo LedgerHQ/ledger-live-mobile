53d50398053890b14883a9f45c8063cd87bb1e04
LFBarreto <lfb.access@gmail.com>
2021-07-30 11:55:21 +0200

    (DS): fix expo build mobile

--- uilib/App.tsx
+++ uilib/App.tsx
@@ -1,1 +1,9 @@
-export { default } from "./storybook";
+import React from "react";
+import { SafeAreaView } from "react-native";
+import StoryBook from "./storybook";
+
+export default () => (
+  <SafeAreaView style={{ flex: 1, marginTop: 50 }}>
+    <StoryBook />
+  </SafeAreaView>
+);
