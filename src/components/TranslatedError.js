52d9084d35559a36889fd38d162527a3b376e6e4
Alexandre Chabrolin <87011321+achabrolin-ledger@users.noreply.github.com>
2021-10-08 11:05:24 +0200

    fixup! fix error wording for different device/platform

--- src/components/TranslatedError.js
+++ src/components/TranslatedError.js
@@ -21,19 +21,30 @@ export default function TranslatedError({ error, field = "title" }: Props) {
   // $FlowFixMe
   const arg: Object = { message: error.message, returnObjects: true, ...error };
   if (error.name) {
-    let translation = t(`errors.${error.name}.${field}`, arg);
-    if (translation !== `errors.${error.name}.${field}`) {
+    // the string id of a simple error message
+    const simpleTranslationStringId = `errors.${error.name}.${field}`;
+
+    let translation = t(simpleTranslationStringId, arg);
+    if (translation !== simpleTranslationStringId) {
       if (typeof translation === "object" && "productName" in arg) {
         // it has specific translation for different device and platform
         const platform = Platform.OS;
         const device = arg.productName.includes("Nano S") ? "nanoS" : "nanoX";
-        translation = t(
-          `errors.${error.name}.${field}.${platform}.${device}`,
-          arg,
-        );
-        if (
-          translation !== `errors.${error.name}.${field}.${platform}.${device}`
-        ) {
+
+        if (platform === "ios" && device === "nanoS") {
+          /**
+           * this case should not happen since you can't nativelly connect a Nano S to an iOS device
+           * (but you can in local/dev by using a proxy with the simulator)
+           */
+          return t(`errors.generic.${field}`, arg);
+        }
+
+        // the string id of a detailled (os and device specific) error message
+        const detailedTranslationStringId = `errors.${error.name}.${field}.${platform}.${device}`;
+
+        translation = t(detailedTranslationStringId, arg);
+
+        if (translation !== detailedTranslationStringId) {
           return translation;
         }
       } else if (typeof translation === "string") {
