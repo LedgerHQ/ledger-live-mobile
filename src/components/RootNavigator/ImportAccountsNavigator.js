5058ed58212dc4191a1f9fba98e34f0caf45fb41
Juan Cortes Ross <juan@bohem.io>
2021-11-16 01:20:48 +0100

    LL-8135 Fix incorrect import for HeaderBackButton

--- src/components/RootNavigator/ImportAccountsNavigator.js
+++ src/components/RootNavigator/ImportAccountsNavigator.js
@@ -40,7 +40,7 @@ export default function ImportAccountsNavigator() {
         component={DisplayResult}
         options={{
           title: t("account.import.result.title"),
-          headerLeft: null,
+          headerLeft: () => <BackButton />,
         }}
       />
       <Stack.Screen
