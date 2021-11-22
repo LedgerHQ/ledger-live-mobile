da0c2313c76f1995dc26535f4b12bba9499ba12d
Gabriel Soares <91940736+gsoares-ledger@users.noreply.github.com>
2021-11-22 10:39:12 +0100

    Rebranding of the bottom modal and bump native ui library version (#1983)

---
+++ src/components/BottomModal.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { StyleProp, ViewStyle } from "react-native";
+import { BottomDrawer } from "@ledgerhq/native-ui";
+import ButtonUseTouchable from "../context/ButtonUseTouchable";
+
+let isModalOpenedref: boolean | undefined = false;
+
+export type Props = {
+  id?: string;
+  isOpened?: boolean;
+  onClose?: () => void;
+  onModalHide?: () => void;
+  children?: React.ReactNode;
+  style?: StyleProp<ViewStyle>;
+  preventBackdropClick?: boolean;
+  containerStyle?: StyleProp<ViewStyle>;
+};
+
+const BottomModal = ({
+  isOpened,
+  onClose = () => {},
+  children,
+  style,
+  preventBackdropClick,
+  onModalHide,
+  containerStyle,
+  ...rest
+}: Props) => {
+  const [open, setIsOpen] = useState(false);
+
+  // workarround to make sure no double modal can be opened at same time
+  useEffect(
+    () => () => {
+      isModalOpenedref = false;
+    },
+    [],
+  );
+
+  useEffect(() => {
+    if (!!isModalOpenedref && isOpened) {
+      onClose();
+    } else {
+      setIsOpen(isOpened ?? false);
+    }
+    isModalOpenedref = isOpened;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isOpened]); // do not add onClose it might cause some issues on modals ie: filter manager modal
+
+  return (
+    <ButtonUseTouchable.Provider value={true}>
+      <BottomDrawer
+        preventBackdropClick={preventBackdropClick}
+        isOpen={open}
+        onClose={onClose}
+        modalStyle={style}
+        containerStyle={containerStyle}
+        onModalHide={() => onModalHide && onModalHide()}
+        {...rest}
+      >
+        {children}
+      </BottomDrawer>
+    </ButtonUseTouchable.Provider>
+  );
+};
+
+export default BottomModal;
