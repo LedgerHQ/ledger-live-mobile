c6c32dee485c51144a6f09a8c2e01e9891f2e92f
LFBarreto <lfb.access@gmail.com>
2021-11-24 14:16:22 +0100

    (Locales): fix missing de language import

--- src/locales/index.js
+++ src/locales/index.js
@@ -3,3 +3,4 @@ exports.fr = { common: require("./fr/common.json"), };
 exports.es = { common: require("./es/common.json"), };
 exports.zh = { common: require("./zh/common.json"), };
 exports.ru = { common: require("./ru/common.json"), };
+exports.de = { common: require("./de/common.json"), };
