4063481b3226d20f954ce49c60e53fc9cf87e445
nparigi-ledger <nicolas.parigi@ledger.fr>
2021-11-24 16:49:53 +0100

    LL-8031 - Refactor and redesign whole notification center (news, status, snackbar)

---
+++ src/components/RootNavigator/NotificationCenterNavigator.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import {
+  createMaterialTopTabNavigator,
+  MaterialTopTabBarProps,
+} from "@react-navigation/material-top-tabs";
+import { useTranslation } from "react-i18next";
+import { useAnnouncements } from "@ledgerhq/live-common/lib/notifications/AnnouncementProvider";
+import { ScreenName } from "../../const";
+import NotificationCenterNews from "../../screens/NotificationCenter/News";
+import NotificationCenterStatus from "../../screens/NotificationCenter/Status";
+import { NavigationToggle } from "@ledgerhq/native-ui/components/Navigation/ToggleGroup/NavigationToggleGroup";
+import { ToggleGroupContainer } from "@ledgerhq/native-ui/components/Navigation/ToggleGroup/TemplateToggleGroup";
+import { Flex } from "@ledgerhq/native-ui";
+import styled from "styled-components/native";
+
+const Tab = createMaterialTopTabNavigator();
+
+const TabBarContainer = styled(Flex)`
+  border-bottom-width: 1px;
+  border-bottom-color: ${p => p.theme.colors.palette.neutral.c40};
+`;
+
+function TabBar({ state, descriptors, navigation }: MaterialTopTabBarProps) {
+  return (
+    <TabBarContainer paddingLeft={4} paddingRight={4} paddingBottom={4}>
+      <ToggleGroupContainer>
+        {state.routes.map((route, index) => {
+          const { options } = descriptors[route.key];
+          const label = options.title;
+
+          const isActive = state.index === index;
+
+          const onPress = () => {
+            const event = navigation.emit({
+              type: "tabPress",
+              target: route.key,
+              canPreventDefault: true,
+            });
+
+            if (!isActive && !event.defaultPrevented) {
+              navigation.navigate(route.name);
+            }
+          };
+
+          return (
+            <NavigationToggle
+              label={label || ""}
+              isActive={isActive}
+              index={index}
+              onPress={onPress}
+            />
+          );
+        })}
+      </ToggleGroupContainer>
+    </TabBarContainer>
+  );
+}
+
+export default function NotificationCenterNavigator() {
+  const { t } = useTranslation();
+  const { allIds, seenIds } = useAnnouncements();
+  const [notificationsCount] = useState(allIds.length - seenIds.length);
+
+  // Fixme Typescript: Update react-native-tab-view to 3.1.1 to remove Tab.navigator ts error
+  return (
+    <>
+      {/* @ts-ignore */}
+      <Tab.Navigator tabBar={props => <TabBar {...props} />}>
+        <Tab.Screen
+          name={ScreenName.NotificationCenterNews}
+          component={NotificationCenterNews}
+          options={{
+            title: t(
+              notificationsCount > 0
+                ? "notificationCenter.news.titleCount"
+                : "notificationCenter.news.title",
+              {
+                count: notificationsCount,
+              },
+            ),
+          }}
+        />
+        <Tab.Screen
+          name={ScreenName.NotificationCenterStatus}
+          component={NotificationCenterStatus}
+          options={{
+            title: t("notificationCenter.status.title"),
+          }}
+        />
+      </Tab.Navigator>
+    </>
+  );
+}
