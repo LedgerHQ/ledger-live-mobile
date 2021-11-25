983a5fd16f4253c3a9aeef1be2ef7583023a7f98
Thomas Laforge <thomas.laforge@ledger.fr>
2021-11-25 10:35:32 +0100

    adding format function

--- src/components/ByteSize.js
+++ src/components/ByteSize.js
@@ -6,33 +6,38 @@ import type { DeviceModel } from "@ledgerhq/devices";
 const k = 1024; // 1kb unit
 const sizes = ["bytes", "kbUnit", "mbUnit"];
 
-/** formats a byte value into its correct size in kb or mb unit takling in account the device block size */
+/** formats a byte value into its correct size in kb or mb unit talking in account the device block size */
 const ByteSize = ({
   value,
   deviceModel,
-  decimals = 2,
+  decimals = 0,
   firmwareVersion,
+  formatFunction
 }: {
   value: number,
   deviceModel: DeviceModel,
   decimals?: number,
   firmwareVersion: string,
+  formatFunction?: (val: number) => number
 }) => {
   if (!value) return "–";
 
   const blockSize = deviceModel.getBlockSize(firmwareVersion);
+
+  // FIXME it should be on live-common side
   const bytes = Math.ceil(value / blockSize) * blockSize;
 
   const i = Math.floor(Math.log(bytes) / Math.log(k));
 
   const dm = Math.max(0, decimals);
+  const pureSize = parseFloat(bytes / Math.pow(k, i));
+  const formattedSize = formatFunction ? formatFunction(pureSize) : pureSize
+  const size = formattedSize.toFixed(dm)
 
   return (
     <Trans
       i18nKey={`byteSize.${sizes[i]}`}
-      values={{
-        size: parseFloat((bytes / k ** i).toFixed(dm)),
-      }}
+      values={{ size }}
     />
   );
 };
