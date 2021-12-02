a569feb8a208e1c3bd5c2210157a26d6078c478f
jelbaz-ledger <julien.el-baz@ledger.fr>
2021-12-02 12:44:20 +0100

    rebrand: improve the onboarding language modal transitions and flow

--- src/components/NavigationHeader.tsx
+++ src/components/NavigationHeader.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { Pressable, View } from "react-native";
+import { View } from "react-native";
 import { ArrowLeftMedium, CloseMedium } from "@ledgerhq/native-ui/assets/icons";
 import { Flex, Text, Link } from "@ledgerhq/native-ui";
 import { StackHeaderProps } from "@react-navigation/stack";
 import { getHeaderTitle } from "@react-navigation/elements";
 
-// Magic number
-const HEADER_HEIGHT = 52;
-
 function NavigationHeader({
   navigation,
-  layout,
   route,
   options,
   back,
@@ -20,36 +16,31 @@ function NavigationHeader({
   const title = t(getHeaderTitle(options, route.name));
 
   return (
-    <Flex height={layout.height / 10 + HEADER_HEIGHT} justifyContent="flex-end">
-      <Pressable
-        style={{ flex: 1 }}
+    <Flex
+      flexDirection="row"
+      justifyContent="space-between"
+      alignItems="center"
+      backgroundColor="palette.neutral.c00"
+      py={6}
+      mb={6}
+    >
+      {back ? (
+        <Link size="large" Icon={ArrowLeftMedium} onPress={navigation.goBack} />
+      ) : (
+        <View />
+      )}
+      <Text variant="large" fontWeight="semiBold">
+        {title}
+      </Text>
+      <Link
+        size="large"
+        Icon={CloseMedium}
         onPress={() => {
-          navigation.canGoBack() && navigation.goBack();
+          navigation.getParent()?.goBack();
         }}
       />
-      <Flex
-        flexDirection="row"
-        justifyContent="space-between"
-        alignItems="center"
-        backgroundColor="palette.neutral.c00"
-        p={6}
-      >
-        {back ? (
-          <Link
-            size="large"
-            Icon={ArrowLeftMedium}
-            onPress={navigation.goBack}
-          />
-        ) : (
-          <View />
-        )}
-        <Text variant="large" fontWeight="semiBold">
-          {title}
-        </Text>
-        <Link size="large" Icon={CloseMedium} onPress={navigation.popToTop} />
-      </Flex>
     </Flex>
   );
 }
 
-export default NavigationHeader;
+export default (props: StackHeaderProps) => <NavigationHeader {...props} />;
--- src/components/NavigationModalContainer.tsx
+++ src/components/NavigationModalContainer.tsx
@@ -1,9 +1,37 @@
+import React from "react";
+import { Pressable, SafeAreaView } from "react-native";
 import { Flex } from "@ledgerhq/native-ui";
+import { StackScreenProps } from "@react-navigation/stack";
 import styled from "styled-components/native";
 
-export default styled(Flex).attrs(p => ({
+export const MIN_MODAL_HEIGHT = 30;
+
+const ScreenContainer = styled(Flex).attrs(p => ({
+  edges: ["bottom"],
   flex: 1,
   backgroundColor: "palette.neutral.c00",
   p: p.p ?? 6,
-  mb: p.mb ?? 4,
 }))``;
+type Props = StackScreenProps<{}> & { children: React.ReactNode };
+
+export default function NavigationModalContainer({
+  navigation,
+  children,
+}: Props) {
+  return (
+    <Flex flex={1}>
+      <Flex height="8%" minHeight={MIN_MODAL_HEIGHT}>
+        <Pressable
+          style={{ flex: 1 }}
+          onPress={() => {
+            navigation.canGoBack() && navigation.goBack();
+          }}
+        />
+      </Flex>
+
+      <ScreenContainer>
+        <SafeAreaView style={{ flex: 1 }}>{children}</SafeAreaView>
+      </ScreenContainer>
+    </Flex>
+  );
+}
