7d8596a5cbe7ac5dcbc7ff9c95b3a5aa8022ddb4
NickolaiYurchenko <nickolai.yurchenko@gmail.com>
2021-12-16 16:04:21 +0200

    removed unnecessary dep

--- src/components/RootNavigator/BuyNavigator.js
+++ src/components/RootNavigator/BuyNavigator.js
@@ -2,14 +2,11 @@
 
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { useTranslation } from "react-i18next";
 import { ScreenName } from "../../const";
 import Buy from "../../screens/Exchange/Buy";
 import Coinify from "../../screens/Exchange/Coinify";
 
 export default function BuyNavigator() {
-  const { t } = useTranslation();
-
   return (
     <Stack.Navigator
       screenOptions={{
