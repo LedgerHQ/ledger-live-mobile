eabb43fd2d875d376cbd2761a6c2d5ce3ddf82b1
Gabriel R. Soares <gabriel.soares@ledger.fr>
2022-01-10 14:25:58 +0100

    Polish PanAndZoomView code and add comments

--- src/components/PanAndZoomView.js
+++ src/components/PanAndZoomView.js
@@ -9,35 +9,64 @@ import {
 } from "react-native-gesture-handler";
 import { Animated } from "react-native";
 
-
 const PanAndZoomView = ({ children }) => {
-  const animatedScale = useRef(new Animated.Value(1)).current;
-  const previousScaleOffset = useRef(1);
-  const currentScaleOffset = useRef(new Animated.Value(1)).current;
-  const handlePinch = Animated.event([{ nativeEvent: { scale: animatedScale } }], {
-        useNativeDriver: true,
-      });
-  const scale = Animated.multiply(animatedScale, currentScaleOffset);
+  // normal number used to store the current scale and update the corresponding animated value
+  // after the pinch gesture is finished
+  const scaleOffset = useRef(1);
+  // animated value used to store the current scale after the pinch gesture is finished
+  const animatedScaleOffset = useRef(new Animated.Value(1)).current;
+  // animated value to store the scale during the pinch gesture (which always starts at 1)
+  const pinchScale = useRef(new Animated.Value(1)).current;
+  // binding of the pinchScale value to the pinch gesture scale
+  const handlePinch = Animated.event([{ nativeEvent: { scale: pinchScale } }], {
+    useNativeDriver: true,
+  });
+  // actual scale of the image at any given moment (during the pinch gesture and after)
+  const scale = Animated.multiply(pinchScale, animatedScaleOffset);
 
-  const previousTranslateXOffset = useRef(0);
-  const currentTranslateXOffset = useRef(new Animated.Value(1)).current;
-  const translateX = useRef(new Animated.Value(0)).current;
+  // normal number used to store the current translate X and update the corresponding animated value
+  // after the pan gesture is finished
+  const translateXOffset = useRef(0);
+  // animated value used to store the current translate X after the pan gesture is finished
+  const animatedTranslateXOffset = useRef(new Animated.Value(0)).current;
+  // animated value to store the translate X during the pan gesture (which always starts at 0)
+  const panTranslateX = useRef(new Animated.Value(0)).current;
+  // actual translate X of the image at any given moment (during the pan gesture and after)
+  const translateX = Animated.add(
+    animatedTranslateXOffset,
+    Animated.divide(panTranslateX, scale),
+  );
 
-  const previousTranslateYOffset = useRef(0);
-  const currentTranslateYOffset = useRef(new Animated.Value(1)).current;
-  const translateY = useRef(new Animated.Value(0)).current;
+  // normal number used to store the current translate Y and update the corresponding animated value
+  // after the pan gesture is finished
+  const translateYOffset = useRef(0);
+  // animated value used to store the current translate Y after the pan gesture is finished
+  const animatedTranslateYOffset = useRef(new Animated.Value(0)).current;
+  // animated value to store the translate Y during the pan gesture (which always starts at 0)
+  const panTranslateY = useRef(new Animated.Value(0)).current;
+  // actual translate Y of the image at any given moment (during the pan gesture and after)
+  const translateY = Animated.add(
+    animatedTranslateYOffset,
+    Animated.divide(panTranslateY, scale),
+  );
 
+  // binding of the panTranslateX and panTranslateY values to the pan gesture translations
   const handlePan = Animated.event(
-        [
-          {
-            nativeEvent: { translationX: translateX, translationY: translateY },
-          },
-        ],
-        {
-          useNativeDriver: true,
+    [
+      {
+        nativeEvent: {
+          translationX: panTranslateX,
+          translationY: panTranslateY,
         },
-      );
+      },
+    ],
+    {
+      useNativeDriver: true,
+    },
+  );
 
+  // we restrict pinch gestures to two fingers and panning gesture to one finger otherwise they 
+  // conflict with each other and we get weird behaviour on Android
   return (
     <PinchGestureHandler
       onGestureEvent={handlePinch}
@@ -45,13 +74,16 @@ const PanAndZoomView = ({ children }) => {
       maxPointers={2}
       onHandlerStateChange={e => {
         if (e.nativeEvent.state === State.END) {
-          // this is to prevent the image from jumping back to the original size
-          previousScaleOffset.current *= e.nativeEvent.scale;
-          animatedScale.setValue(1);
-          currentScaleOffset.setValue(previousScaleOffset.current);
+          // stores the current scale
+          scaleOffset.current *= e.nativeEvent.scale;
+          animatedScaleOffset.setValue(scaleOffset.current);
+          // resets the pinch scale to 1 once the gesture is finished
+          pinchScale.setValue(1);
         }
       }}
     >
+      {/* each gesture handler can only update 1 view, so we need one for
+       scaling and another for panning, this is the scaling view */}
       <Animated.View
         style={[
           {
@@ -64,48 +96,39 @@ const PanAndZoomView = ({ children }) => {
           minPointers={1}
           maxPointers={1}
           onHandlerStateChange={e => {
-            console.log(e.nativeEvent);
-            console.log("Before", previousTranslateXOffset.current);
             if (e.nativeEvent.state === State.END) {
-              previousTranslateXOffset.current += e.nativeEvent.translationX / previousScaleOffset.current;
-              currentTranslateXOffset.setValue(
-                previousTranslateXOffset.current,
-              );
-              translateX.setValue(0);
+              // stores the current translate X
+              translateXOffset.current +=
+                e.nativeEvent.translationX / scaleOffset.current;
+              animatedTranslateXOffset.setValue(translateXOffset.current);
+              // resets the pan translate X to 0 once the gesture is finished
+              panTranslateX.setValue(0);
 
-              previousTranslateYOffset.current += e.nativeEvent.translationY / previousScaleOffset.current;
-              currentTranslateYOffset.setValue(
-                previousTranslateYOffset.current,
-              );
-              translateY.setValue(0);
+              // stores the current translate Y
+              translateYOffset.current +=
+                e.nativeEvent.translationY / scaleOffset.current;
+              animatedTranslateYOffset.setValue(translateYOffset.current);
+              // resets the pan translate Y to 0 once the gesture is finished
+              panTranslateY.setValue(0);
             }
-            console.log("After", {
-              previousTranslateXOffset: previousTranslateXOffset.current,
-              currentTranslateXOffset,
-            });
           }}
         >
+          {/* translating view */}
           <Animated.View
-            style={
-              [{
+            style={[
+              {
                 transform: [
                   {
-                    translateX: Animated.add(
-                      currentTranslateXOffset,
-                      Animated.divide(translateX, scale),
-                    ),
+                    translateX,
                   },
                   {
-                    translateY: Animated.add(
-                      currentTranslateYOffset,
-                      Animated.divide(translateY, scale),
-                    ),
+                    translateY,
                   },
                 ],
               },
             ]}
           >
-              {children}            
+            {children}
           </Animated.View>
         </PanGestureHandler>
       </Animated.View>
