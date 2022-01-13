f6c561e974ed70279ec6c62c5dc87288ad15fd87
elbywan <julien.el-baz@ledger.fr>
2022-01-13 09:54:30 +0100

    attempt to fix ci builds

--- metro.config.js
+++ metro.config.js
@@ -6,11 +6,24 @@
  */
 /* eslint-disable import/no-extraneous-dependencies */
 
+const path = require("path");
 const defaultSourceExts = require("metro-config/src/defaults/defaults")
   .sourceExts;
 const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks");
 
+// Emulate what the ./node_modules/.bin/react-native binary is doing by adding node paths manually…
+// This is needed because the react native prod build scripts call react-native/cli.js
+// which does not set these paths.
+const nodeModulesPaths = [
+  path.join(__dirname, "node_modules"),
+  path.join(__dirname, "node_modules/.pnpm/node_modules"),
+];
+process.env.NODE_PATH = `${process.env.NODE_PATH || ""}:${nodeModulesPaths.join(
+  ":",
+)}`;
+
 module.exports = {
+  projectRoot: __dirname,
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -20,14 +33,15 @@ module.exports = {
     }),
   },
   resolver: {
+    nodeModulesPaths,
     // MetroSymlinksResolver resolves symlinked dependencies which maks metro play along nice with pnpm.
     // In practice, it is just a bit more complicated than that unfortunately…
     resolveRequest: MetroSymlinksResolver({
-      remapModule: (_context, moduleName, _platform) => {
+      remapModule: (context, moduleName, _platform) => {
         // MetroSymlinksResolver produces the incorrect "./dist/npm/common/npm/common/wswrapper.js" resolution here.
         // It's like it adds up the package.json "browser" + "react-native" fields paths…
         if (
-          _context.originModulePath.includes("ripple-lib@1.10.0") &&
+          context.originModulePath.includes("ripple-lib@1.10.0") &&
           moduleName === "./dist/npm/common/wswrapper.js"
         ) {
           return "./wswrapper.js";
@@ -36,7 +50,7 @@ module.exports = {
         // "package.js" contains "module.meta" calls that will not work with the react-native env.
         // To solve this replace with "packageInfo.cjs" which is safe.
         if (
-          _context.originModulePath.includes("@polkadot") &&
+          context.originModulePath.includes("@polkadot") &&
           moduleName === "./packageInfo.js"
         ) {
           return "./packageInfo.cjs";
