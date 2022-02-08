8aab91c9f8db747c1986e74513784c38a124df76
Come Grellard <come.grellard@ledger.fr>
2022-02-08 10:13:34 +0100

    raw images removed

--- src/images/devices/NanoS.tsx
+++ src/images/devices/NanoS.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import Svg, { Path } from "react-native-svg";
 
 const NanoS = ({ color }: { color: string }) => (
--- src/images/devices/NanoX.tsx
+++ src/images/devices/NanoX.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import Svg, { Path } from "react-native-svg";
 
 const NanoX = ({ color }: { color: string }) => (
