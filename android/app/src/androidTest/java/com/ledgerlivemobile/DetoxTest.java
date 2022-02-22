71d53e02ca1a1a7a26c194117549f9d9da527741
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2022-02-22 19:05:04 +0100

    Add CI config + test butler on android

--- android/app/src/androidTest/java/com/ledgerlivemobile/DetoxTest.java
+++ android/app/src/androidTest/java/com/ledgerlivemobile/DetoxTest.java
@@ -10,6 +10,8 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
+import com.linkedin.android.testbutler.TestButler;
+
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class DetoxTest {
@@ -19,6 +21,7 @@ public class DetoxTest {
 
     @Test
     public void runDetoxTests() {
+        TestButlerProbe.assertReadyIfInstalled();
         Detox.runTests(mActivityRule);
     }
 }
