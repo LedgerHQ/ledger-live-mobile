5b859ace31f6ae5a6cef7b00cd0bcfaf61fe2265
nparigi-ledger <89014981+nparigi-ledger@users.noreply.github.com>
2022-03-06 14:03:22 +0100

    LIVE-1535 - V3 Accounts list (assets)  (#2263)
    * Rebrand account list (assets) + portfolio assets section
    
    * Update native

--- src/components/RootNavigator/AccountsNavigator.tsx
+++ src/components/RootNavigator/AccountsNavigator.tsx
@@ -1,11 +1,8 @@
 import React, { useMemo } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { useTranslation } from "react-i18next";
 import { useTheme } from "styled-components/native";
 import { ScreenName } from "../../const";
 import Accounts from "../../screens/Accounts";
-import AccountOrder from "../../screens/Accounts/AccountOrder";
-import AddAccount from "../../screens/Accounts/AddAccount";
 import Account from "../../screens/Account";
 import NftCollection from "../../screens/Nft/NftCollection";
 import NftGallery from "../../screens/Nft/NftGallery";
@@ -16,7 +13,6 @@ import AccountHeaderRight from "../../screens/Account/AccountHeaderRight";
 import AccountHeaderTitle from "../../screens/Account/AccountHeaderTitle";
 
 export default function AccountsNavigator() {
-  const { t } = useTranslation();
   const { colors } = useTheme();
   const stackNavConfig = useMemo(() => getStackNavigatorConfig(colors), [
     colors,
@@ -27,9 +23,7 @@ export default function AccountsNavigator() {
         name={ScreenName.Accounts}
         component={Accounts}
         options={{
-          title: t("accounts.title"),
-          headerLeft: () => <AccountOrder />,
-          headerRight: () => <AddAccount />,
+          headerShown: false,
         }}
       />
       <Stack.Screen
