dacced446700e6f55c41eeb70465cec3ce0a2ed0
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2022-03-11 20:08:10 +0100

    Revert removing internal state for switch as props seem to not be updated properly

--- src/screens/Settings/Experimental/FeatureSwitch.js
+++ src/screens/Settings/Experimental/FeatureSwitch.js
@@ -21,14 +21,19 @@ export default class FeatureSwitch extends PureComponent<Props, State> {
     valueOn: true,
     valueOff: false,
   };
+  state = {
+    checked: this.props.checked || false,
+  };
 
   onChange = (evt: boolean) => {
     const { onChange, valueOn, valueOff, name } = this.props;
     onChange(name, evt ? valueOn : valueOff);
+    this.setState({ checked: evt });
   };
 
   render() {
-    const { checked = false, name, readOnly } = this.props;
+    const { name, readOnly } = this.props;
+    const { checked } = this.state;
     return (
       <>
         <Track
