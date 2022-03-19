8722ba07ca0d6cf558a177136a68c321cabaf453
nparigi-ledger <89014981+nparigi-ledger@users.noreply.github.com>
2022-03-19 13:15:25 +0100

    V3 - Trow flow rebrand - Votes (row, modal ...) (#2305)

--- src/families/tron/Votes/Row.tsx
+++ src/families/tron/Votes/Row.tsx
@@ -1,15 +1,14 @@
 import React, { useCallback } from "react";
-import { View, Linking, StyleSheet, TouchableOpacity } from "react-native";
+import { Linking, TouchableOpacity } from "react-native";
 
 import { getAddressExplorer } from "@ledgerhq/live-common/lib/explorers";
 
 import { ExplorerView } from "@ledgerhq/live-common/lib/types";
 
-import { Flex, Text } from "@ledgerhq/native-ui";
+import { Box, BoxedIcon, Flex, Text } from "@ledgerhq/native-ui";
 import { useTheme } from "styled-components/native";
+import { MedalMedium, TrophyMedium } from "@ledgerhq/native-ui/assets/icons";
 import Clock from "../../../icons/Clock";
-import Trophy from "../../../icons/Trophy";
-import Medal from "../../../icons/Medal";
 
 type Props = {
   validator: any;
@@ -28,7 +27,6 @@ const Row = ({
   duration,
   explorerView,
   isSR,
-  isLast = false,
 }: Props) => {
   const { colors } = useTheme();
   const srURL = explorerView && getAddressExplorer(explorerView, address);
@@ -38,23 +36,17 @@ const Row = ({
   }, [srURL]);
 
   return (
-    <View style={styles.root}>
-      <View style={styles.row}>
-        <View
-          style={[
-            styles.icon,
-            !isSR
-              ? { backgroundColor: colors.lightFog }
-              : { backgroundColor: colors.lightLive },
-          ]}
+    <Flex flexDirection={"column"} alignItems={"center"} py={4}>
+      <Flex flexDirection={"row"} alignItems={"center"}>
+        <Box mr={4}>
+          <BoxedIcon Icon={isSR ? TrophyMedium : MedalMedium} />
+        </Box>
+        <Flex
+          flexDirection={"column"}
+          alignItems={"flex-start"}
+          justifyContent={"flex-end"}
+          flex={1}
         >
-          {isSR ? (
-            <Trophy size={16} color={colors.live} />
-          ) : (
-            <Medal size={16} color={colors.grey} />
-          )}
-        </View>
-        <View style={styles.labelContainer}>
           <TouchableOpacity onPress={openSR}>
             <Text
               variant={"body"}
@@ -71,70 +63,15 @@ const Row = ({
               {duration}
             </Text>
           </Flex>
-        </View>
-        <View style={[styles.labelContainer, styles.labelContainerRight]}>
+        </Flex>
+        <Flex flexDirection={"column"} alignItems={"center"}>
           <Text variant={"paragraph"} color={"neutral.c80"} ml={2}>
             {amount}
           </Text>
-        </View>
-      </View>
-      {!isLast && (
-        <View
-          style={[styles.separator, { backgroundColor: colors.lightFog }]}
-        />
-      )}
-    </View>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 };
-
-const styles = StyleSheet.create({
-  root: {
-    flexDirection: "column",
-    alignItems: "center",
-    paddingTop: 12,
-  },
-  separator: {
-    height: 1,
-    width: "100%",
-    marginTop: 12,
-  },
-  row: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  icon: {
-    alignItems: "center",
-    justifyContent: "center",
-    width: 36,
-    height: 36,
-    borderRadius: 5,
-    marginRight: 12,
-  },
-  title: {
-    fontSize: 14,
-    lineHeight: 16,
-    paddingBottom: 4,
-  },
-  labelContainer: {
-    flexDirection: "column",
-    alignItems: "flex-start",
-    justifyContent: "flex-end",
-    flex: 1,
-  },
-  labelContainerRight: {
-    alignItems: "flex-end",
-    marginLeft: 10,
-    flexShrink: 1,
-    flex: 0,
-  },
-  label: {
-    fontSize: 13,
-    marginLeft: 6,
-  },
-  durationContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-});
 
 export default Row;
