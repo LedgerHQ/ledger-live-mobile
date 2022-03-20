dcca4ba1583d04cf8e0190cd0d15e638abb54299
Vitalii <takur12345@gmail.com>
2022-03-20 17:32:45 +0200

    LIVE-1711: Migrate from the old live app provider to the new one

--- src/screens/Platform/App.js
+++ src/screens/Platform/App.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React from "react";
-import { usePlatformApp } from "@ledgerhq/live-common/lib/platform/PlatformAppProvider";
+import { useRemoteLiveAppManifest } from "@ledgerhq/live-common/lib/platform/providers/RemoteLiveAppProvider";
 import type { StackScreenProps } from "@react-navigation/stack";
 import { useTheme } from "@react-navigation/native";
 import TrackScreen from "../../analytics/TrackScreen";
@@ -10,8 +10,7 @@ import WebPlatformPlayer from "../../components/WebPlatformPlayer";
 const PlatformApp = ({ route }: StackScreenProps) => {
   const { dark } = useTheme();
   const { platform: appId, ...params } = route.params;
-  const { manifests } = usePlatformApp();
-  const manifest = manifests.get(appId);
+  const manifest = useRemoteLiveAppManifest(appId);
   const themeType = dark ? "dark" : "light";
 
   return manifest ? (
