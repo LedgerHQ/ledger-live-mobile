cb97d6f9d293c1911409725c5ffc971d8ccaaa34
Come Grellard <come.grellard@ledger.fr>
2022-04-14 11:00:42 +0200

    wip

--- src/components/RatingsModal.tsx
+++ src/components/RatingsModal.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useCallback } from "react";
 import { Flex, Text, Button } from "@ledgerhq/native-ui";
 import { WebView } from "react-native-webview";
-import InAppReview from 'react-native-in-app-review';
+import InAppReview from "react-native-in-app-review";
 import BottomModal from "./BottomModal";
 
 const RatingsModal = () => {
@@ -10,9 +10,46 @@ const RatingsModal = () => {
     setIsOpened(false);
   }, [setIsOpened]);
 
-  const onRate = useCallback(() => {
-    const isRatingAvailable = InAppReview.isAvailable();
-    console.log("Rating", isRatingAvailable);
+  const onRate = useCallback(async () => {
+    try {
+      const isRatingAvailable = InAppReview.isAvailable();
+      console.log("Rating", isRatingAvailable);
+      const hasFlowFinishedSuccessfully = await InAppReview.RequestInAppReview();
+
+      // when return true in android it means user finished or close review flow
+      console.log("InAppReview in android", hasFlowFinishedSuccessfully);
+
+      // when return true in ios it means review flow lanuched to user.
+      console.log(
+        "InAppReview in ios has launched successfully",
+        hasFlowFinishedSuccessfully,
+      );
+
+      // 1- you have option to do something ex: (navigate Home page) (in android).
+      // 2- you have option to do something,
+      // ex: (save date today to lanuch InAppReview after 15 days) (in android and ios).
+
+      // 3- another option:
+      if (hasFlowFinishedSuccessfully) {
+        // do something for ios
+        // do something for android
+      }
+
+      // for android:
+      // The flow has finished. The API does not indicate whether the user
+      // reviewed or not, or even whether the review dialog was shown. Thus, no
+      // matter the result, we continue our app flow.
+
+      // for ios
+      // the flow lanuched successfully, The API does not indicate whether the user
+      // reviewed or not, or he/she closed flow yet as android, Thus, no
+      // matter the result, we continue our app flow.
+    } catch (error) {
+      // we continue our app flow.
+      // we have some error could happen while lanuching InAppReview,
+      // Check table for errors and code number that can return in catch.
+      console.log(error);
+    }
   }, []);
 
   console.log("Hello Ratings Modal");
