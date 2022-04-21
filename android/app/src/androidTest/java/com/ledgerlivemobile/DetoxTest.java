70370d8b6e3efb18a6c7d512a11222f0532c1628
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2022-04-21 15:50:20 +0200

    added git config to detox java file

--- android/app/src/androidTest/java/com/ledgerlivemobile/DetoxTest.java
+++ android/app/src/androidTest/java/com/ledgerlivemobile/DetoxTest.java
@@ -1,6 +1,7 @@
 package com.ledger.live;
 
 import com.wix.detox.Detox;
+import com.wix.detox.config.DetoxConfig;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -19,6 +20,10 @@ public class DetoxTest {
 
     @Test
     public void runDetoxTests() {
-        Detox.runTests(mActivityRule);
+        // TestButlerProbe.assertReadyIfInstalled();
+
+        DetoxConfig detoxConfig = new DetoxConfig();
+
+        Detox.runTests(mActivityRule, detoxConfig);
     }
 }
