8da9958599dd41ef17f942de0af9fa8318b8c59c
nparigi-ledger <nicolas.parigi@ledger.fr>
2022-04-22 17:54:37 +0200

    Adding and testing react-native-tor module

--- src/components/LoadingApp.js
+++ src/components/LoadingApp.js
@@ -1,3 +1,39 @@
 // @flow
+import React, { useState, useCallback } from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  Image,
+  ImageProps,
+  ImageSourcePropType,
+} from "react-native";
+import TorLogo from "./tor.png";
 
-export default () => null;
+export default () => (
+  <View
+    style={{
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+    }}
+  >
+    <Image
+      source={TorLogo}
+      resizeMode="contain"
+      style={{ width: 70, height: 70, marginBottom: 32 }}
+    />
+    <Text style={styles.innerText}>{"Connecting to Tor Network..."}</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  baseText: {
+    fontWeight: "bold",
+  },
+  innerText: {
+    color: "black",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
--- src/context/LedgerStore.js
+++ src/context/LedgerStore.js
@@ -3,12 +3,21 @@ import React, { Component } from "react";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { createStore, applyMiddleware, compose } from "redux";
+import Tor from "react-native-tor";
 import { getAccounts, getCountervalues, getSettings, getBle } from "../db";
 import reducers from "../reducers";
 import { importSettings } from "../actions/settings";
 import { importStore as importAccounts } from "../actions/accounts";
 import { importBle } from "../actions/ble";
 import { INITIAL_STATE, supportedCountervalues } from "../reducers/settings";
+const http = require("http");
+const https = require("https");
+
+const axios = require("axios");
+const { SocksProxyAgent } = require("socks-proxy-agent");
+
+// Initialize the module
+const tor = Tor();
 
 // $FlowFixMe
 export const store = createStore(
@@ -50,6 +59,30 @@ export default class LedgerStoreProvider extends Component<
   async init() {
     const bleData = await getBle();
     store.dispatch(importBle(bleData));
+
+    try {
+      console.log("torstart");
+      const proxy = await tor.startIfNotStarted();
+      console.log("prxy", proxy);
+      const response1 = await axios.get("https://api.ipify.org?format=json");
+      console.log("response1", response1.data);
+
+      const agent = new SocksProxyAgent(`socks5h://127.0.0.1:${proxy}`);
+
+      const response2 = await axios({
+        url: "https://api.ipify.org?format=json",
+        proxy: false,
+        httpsAgent: agent,
+        httpAgent: agent,
+      });
+
+      http.globalAgent = agent;
+      https.globalAgent = agent;
+
+      console.log("response21", response2.data);
+    } catch (e) {
+      console.error(e);
+    }
 
     const settingsData = await getSettings();
     if (
