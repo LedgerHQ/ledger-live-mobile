076ea3256cd6d395ff97aca40b36750223ad32ab
Come Grellard <come.grellard@ledger.fr>
2022-04-26 17:56:09 +0200

    wip

--- src/components/RatingsModal.tsx
+++ src/components/RatingsModal.tsx
@@ -6,6 +6,49 @@ import InAppReview from "react-native-in-app-review";
 import BottomModal from "./BottomModal";
 import { urls } from "../config/urls";
 
+const injectedJavaScript = `
+window.activeObserver = null;
+window.addEventListener('message', (event) => {
+  window.ReactNativeWebView.postMessage(event.data);
+});
+window.addEventListener('load', () => {
+  window.ReactNativeWebView.postMessage('form-ready');      
+  setObserver(document.querySelector('div[data-qa-focused="true"]')); 
+});
+function checkSubmit() {
+  const submitButton = document.querySelector('button[data-qa*="submit-button"]');
+  if (submitButton) {
+    submitButton.addEventListener('click', () => {
+      window.ReactNativeWebView.postMessage('form-submit');
+    });
+  }
+}
+function onScreenChange() {
+  window.ReactNativeWebView.postMessage('form-screen-changed');
+  activeObserver.disconnect();
+  setObserver(document.querySelector('div[data-qa-focused="true"]'));
+  checkSubmit();
+}
+function setObserver(element) {
+  activeObserver = new MutationObserver(onScreenChange);
+  const config = { attributes: true };
+  activeObserver.observe(element, config);
+}
+
+true;
+`;
+
+const runFirst = `
+setTimeout(function() {
+  const submitButton = document.querySelector('button[data-qa*="submit-button"]');
+  submitButton.addEventListener('click', () => {
+    window.ReactNativeWebView.postMessage('form-submit');
+  });
+}, 100);
+
+true;
+`;
+
 const RatingsModal = () => {
   const [isOpened, setIsOpened] = useState(true);
   const onClose = useCallback(() => {
@@ -13,7 +56,16 @@ const RatingsModal = () => {
   }, [setIsOpened]);
 
   const goToStore = useCallback(() => {
-    Linking.openURL(Platform.OS === "ios" ? "https://apps.apple.com/app/id1361671700?action=write-review" : urls.playstore);
+    Linking.openURL(
+      Platform.OS === "ios"
+        ? "https://apps.apple.com/app/id1361671700?action=write-review"
+        : urls.playstore,
+    );
+  }, []);
+
+  const onMessage = useCallback(event => {
+    // const { data } = event.nativeEvent;
+    console.log("onMessage: ", event.nativeEvent.data);
   }, []);
 
   const requestInAppReview = useCallback(async () => {
@@ -76,10 +128,22 @@ const RatingsModal = () => {
           Typeform
         </Text>
         <WebView
+          source={{
+            uri: encodeURI(
+              "https://form.typeform.com/to/Jo7gqcB4?typeform-medium=embed-sdk&typeform-medium-version=next&typeform-embed=popup-blank",
+            ),
+          }}
           originWhitelist={["*"]}
-          source={{ uri: "https://form.typeform.com/to/Jo7gqcB4" }}
-          onLoadEnd={(message: any) => console.log("ON LOAD", message)}
-          onMessage={(message: any) => console.log("ON MESSAGE", message)}
+          javaScriptEnabledAndroid={true}
+          injectedJavaScript={runFirst}
+          onLoadEnd={(message: any) =>
+            console.log(
+              "ON LOAD",
+              message.nativeEvent.data,
+              message.nativeEvent,
+            )
+          }
+          onMessage={onMessage}
         />
         <Button type="color" onPress={goToStore}>
           Rate
