ae85cd117fcbbf6427a1a85ab266a0c4d3f1dd0e
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2022-04-28 14:50:36 +0200

    adding logging to timezone and setting timezone to Europe/Paris

--- e2e/global-setup.js
+++ e2e/global-setup.js
@@ -1,8 +1,11 @@
 const detox = require("detox");
 
 async function globalSetup() {
-  console.log("==============> STARTING GLOBAL SETUP");
+  //   console.log(`===============> env: ${process.env.TZ}`);
+  //   console.log("==============> STARTING GLOBAL SETUP");`
+  // process.env.TZ = "UTC";
   await detox.globalInit();
+  console.log(`===============> env: ${process.env.TZ}`);
 }
 
 module.exports = globalSetup;
