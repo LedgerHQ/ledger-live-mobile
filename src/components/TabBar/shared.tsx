128cdc57deea1421244ed51b0c5b3793b9521190
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2022-05-11 11:56:30 +0200

    Fix button vertical position

--- src/components/TabBar/shared.tsx
+++ src/components/TabBar/shared.tsx
@@ -4,7 +4,7 @@ export const HAS_GRADIENT = true;
 export const GRADIENT_HEIGHT = 103;
 
 export const MAIN_BUTTON_SIZE = 64;
-export const MAIN_BUTTON_BOTTOM = 24;
+export const MAIN_BUTTON_BOTTOM = 16;
 
 export const BAR_SAFE_HEIGHT = Math.max(
   HAS_GRADIENT ? GRADIENT_HEIGHT : 0,
