a639c19517f9611e5719ace51dcc9aa7563977ff
Harrison Friia <harrison.friia-ext@ledger.fr>
2022-05-12 14:54:06 -0400

    rebuild extension, update pods

--- ios/LedgerConnect/Resources/dist/background.js
+++ ios/LedgerConnect/Resources/dist/background.js
@@ -1,13 +1,13 @@
 (() => {
-var $17166201e7aeaf64$exports = {};
+var $318647308e3525fd$exports = {};
 "use strict";
-Object.defineProperty($17166201e7aeaf64$exports, "__esModule", {
+Object.defineProperty($318647308e3525fd$exports, "__esModule", {
     value: true
 });
 // MIT © 2017 azu
-var $17166201e7aeaf64$var$hasOwn = Object.prototype.hasOwnProperty;
+var $318647308e3525fd$var$hasOwn = Object.prototype.hasOwnProperty;
 // Object.is ponyfill
-$17166201e7aeaf64$exports.is = function(x, y) {
+$318647308e3525fd$exports.is = function(x, y) {
     if (x === y) return x !== 0 || y !== 0 || 1 / x === 1 / y;
     else return x !== x && y !== y;
 };
@@ -20,7 +20,7 @@ $17166201e7aeaf64$exports.is = function(x, y) {
  *
  * - `customEqual`: function should return true if the `a` value is equal to `b` value.
  * - `debug`: enable debug info to console log. This log will be disable in production build
- */ $17166201e7aeaf64$exports.shallowEqual = function(objectA, objectB, options) {
+ */ $318647308e3525fd$exports.shallowEqual = function(objectA, objectB, options) {
     if (objectA === objectB) return true;
     if (typeof objectA !== "object" || objectA === null) {
         var out;
@@ -36,10 +36,10 @@ $17166201e7aeaf64$exports.is = function(x, y) {
         var out;
         return false;
     }
-    var isEqual = options && typeof options.customEqual === "function" ? options.customEqual : $17166201e7aeaf64$exports.is;
+    var isEqual = options && typeof options.customEqual === "function" ? options.customEqual : $318647308e3525fd$exports.is;
     for(var i = 0; i < keysA.length; i++){
         var key = keysA[i];
-        if (!$17166201e7aeaf64$var$hasOwn.call(objectB, key) || !isEqual(objectA[key], objectB[key])) {
+        if (!$318647308e3525fd$var$hasOwn.call(objectB, key) || !isEqual(objectA[key], objectB[key])) {
             var out;
             return false;
         }
@@ -48,41 +48,41 @@ $17166201e7aeaf64$exports.is = function(x, y) {
 };
 
 
-class $ddce45652d33f7aa$export$d53114150807fd22 {
+class $954e4e4dc54c446a$export$d53114150807fd22 {
     constructor(props){
         this.props = Object.freeze(props);
     }
     equals(vo) {
         if (vo === null || vo === undefined) return false;
         if (vo.props === undefined) return false;
-        return $17166201e7aeaf64$exports.shallowEqual(this.props, vo.props);
+        return $318647308e3525fd$exports.shallowEqual(this.props, vo.props);
     }
 }
 
 
 
 
-let $ebac0c3c7faf13a9$export$8d9ecf8a6190d0ad;
-(function($ebac0c3c7faf13a9$export$8d9ecf8a6190d0ad) {
-    $ebac0c3c7faf13a9$export$8d9ecf8a6190d0ad["ConnectDapp"] = 'connectDapp';
-    $ebac0c3c7faf13a9$export$8d9ecf8a6190d0ad["SignAndSendTransaction"] = 'signAndSendTransaction';
-    $ebac0c3c7faf13a9$export$8d9ecf8a6190d0ad["SignTransaction"] = 'signTransaction';
-    $ebac0c3c7faf13a9$export$8d9ecf8a6190d0ad["SignPersonalMessage"] = 'signPersonalMessage';
-})($ebac0c3c7faf13a9$export$8d9ecf8a6190d0ad || ($ebac0c3c7faf13a9$export$8d9ecf8a6190d0ad = {}));
-let $ebac0c3c7faf13a9$export$a7e1148f0e98483a;
-(function($ebac0c3c7faf13a9$export$a7e1148f0e98483a) {
-    $ebac0c3c7faf13a9$export$a7e1148f0e98483a["ConnectDapp"] = 'eth_requestAccounts';
-    $ebac0c3c7faf13a9$export$a7e1148f0e98483a["SignAndSendTransaction"] = 'eth_sendTransaction';
-    $ebac0c3c7faf13a9$export$a7e1148f0e98483a["SignPersonalMessage"] = 'signPersonalMessage';
-})($ebac0c3c7faf13a9$export$a7e1148f0e98483a || ($ebac0c3c7faf13a9$export$a7e1148f0e98483a = {}));
-let $ebac0c3c7faf13a9$export$6f8959d005e6c8f0;
-(function($ebac0c3c7faf13a9$export$6f8959d005e6c8f0) {
-    $ebac0c3c7faf13a9$export$6f8959d005e6c8f0["ConnectDapp"] = 'solana_getAccount';
-    $ebac0c3c7faf13a9$export$6f8959d005e6c8f0["SignAndSendTransaction"] = 'solana_signAndSendTransaction';
-    $ebac0c3c7faf13a9$export$6f8959d005e6c8f0["SignTransaction"] = 'solana_signTransaction';
-    $ebac0c3c7faf13a9$export$6f8959d005e6c8f0["SignAllTransactions"] = 'solana_signAllTransactions';
-})($ebac0c3c7faf13a9$export$6f8959d005e6c8f0 || ($ebac0c3c7faf13a9$export$6f8959d005e6c8f0 = {}));
-class $ebac0c3c7faf13a9$export$3d1391be359ed341 extends $ddce45652d33f7aa$export$d53114150807fd22 {
+let $49c5a02eaa106cab$export$8d9ecf8a6190d0ad;
+(function($49c5a02eaa106cab$export$8d9ecf8a6190d0ad) {
+    $49c5a02eaa106cab$export$8d9ecf8a6190d0ad["ConnectDapp"] = 'connectDapp';
+    $49c5a02eaa106cab$export$8d9ecf8a6190d0ad["SignAndSendTransaction"] = 'signAndSendTransaction';
+    $49c5a02eaa106cab$export$8d9ecf8a6190d0ad["SignTransaction"] = 'signTransaction';
+    $49c5a02eaa106cab$export$8d9ecf8a6190d0ad["SignPersonalMessage"] = 'signPersonalMessage';
+})($49c5a02eaa106cab$export$8d9ecf8a6190d0ad || ($49c5a02eaa106cab$export$8d9ecf8a6190d0ad = {}));
+let $49c5a02eaa106cab$export$a7e1148f0e98483a;
+(function($49c5a02eaa106cab$export$a7e1148f0e98483a) {
+    $49c5a02eaa106cab$export$a7e1148f0e98483a["ConnectDapp"] = 'eth_requestAccounts';
+    $49c5a02eaa106cab$export$a7e1148f0e98483a["SignAndSendTransaction"] = 'eth_sendTransaction';
+    $49c5a02eaa106cab$export$a7e1148f0e98483a["SignPersonalMessage"] = 'signPersonalMessage';
+})($49c5a02eaa106cab$export$a7e1148f0e98483a || ($49c5a02eaa106cab$export$a7e1148f0e98483a = {}));
+let $49c5a02eaa106cab$export$6f8959d005e6c8f0;
+(function($49c5a02eaa106cab$export$6f8959d005e6c8f0) {
+    $49c5a02eaa106cab$export$6f8959d005e6c8f0["ConnectDapp"] = 'solana_getAccount';
+    $49c5a02eaa106cab$export$6f8959d005e6c8f0["SignAndSendTransaction"] = 'solana_signAndSendTransaction';
+    $49c5a02eaa106cab$export$6f8959d005e6c8f0["SignTransaction"] = 'solana_signTransaction';
+    $49c5a02eaa106cab$export$6f8959d005e6c8f0["SignAllTransactions"] = 'solana_signAllTransactions';
+})($49c5a02eaa106cab$export$6f8959d005e6c8f0 || ($49c5a02eaa106cab$export$6f8959d005e6c8f0 = {}));
+class $49c5a02eaa106cab$export$3d1391be359ed341 extends $954e4e4dc54c446a$export$d53114150807fd22 {
     getID() {
         return this.props.id;
     }
@@ -99,20 +99,20 @@ class $ebac0c3c7faf13a9$export$3d1391be359ed341 extends $ddce45652d33f7aa$export
         return this.props.chain;
     }
     static create(props) {
-        return new $ebac0c3c7faf13a9$export$3d1391be359ed341(props);
+        return new $49c5a02eaa106cab$export$3d1391be359ed341(props);
     }
 }
 
 
-let $f047d79a9426f7d2$export$908ff786c04d5297;
-(function($f047d79a9426f7d2$export$908ff786c04d5297) {
-    $f047d79a9426f7d2$export$908ff786c04d5297["solana"] = "solana";
-    $f047d79a9426f7d2$export$908ff786c04d5297["ethereum"] = "ethereum";
-    $f047d79a9426f7d2$export$908ff786c04d5297["content"] = "content";
-    $f047d79a9426f7d2$export$908ff786c04d5297["background"] = "background";
-    $f047d79a9426f7d2$export$908ff786c04d5297["popup"] = "popup";
-})($f047d79a9426f7d2$export$908ff786c04d5297 || ($f047d79a9426f7d2$export$908ff786c04d5297 = {}));
-async function $f047d79a9426f7d2$export$61027ddcc22c3df2(message) {
+let $479e784236c1fcf9$export$908ff786c04d5297;
+(function($479e784236c1fcf9$export$908ff786c04d5297) {
+    $479e784236c1fcf9$export$908ff786c04d5297["solana"] = "solana";
+    $479e784236c1fcf9$export$908ff786c04d5297["ethereum"] = "ethereum";
+    $479e784236c1fcf9$export$908ff786c04d5297["content"] = "content";
+    $479e784236c1fcf9$export$908ff786c04d5297["background"] = "background";
+    $479e784236c1fcf9$export$908ff786c04d5297["popup"] = "popup";
+})($479e784236c1fcf9$export$908ff786c04d5297 || ($479e784236c1fcf9$export$908ff786c04d5297 = {}));
+async function $479e784236c1fcf9$export$61027ddcc22c3df2(message) {
     try {
         return browser.runtime.sendNativeMessage('application.id', message);
     } catch (error) {
@@ -124,10 +124,10 @@ async function $f047d79a9426f7d2$export$61027ddcc22c3df2(message) {
         };
     }
 }
-function $f047d79a9426f7d2$export$8c67b4fe4bab8610(request) {
+function $479e784236c1fcf9$export$8c67b4fe4bab8610(request) {
     let message = {
-        sender: $f047d79a9426f7d2$export$908ff786c04d5297.popup,
-        destination: $f047d79a9426f7d2$export$908ff786c04d5297.content,
+        sender: $479e784236c1fcf9$export$908ff786c04d5297.popup,
+        destination: $479e784236c1fcf9$export$908ff786c04d5297.content,
         request: request
     };
     browser.tabs.query({
@@ -140,37 +140,37 @@ function $f047d79a9426f7d2$export$8c67b4fe4bab8610(request) {
 }
 
 
-const $1feaf5137e56a072$export$2b1d391cd05230fd = (fileName)=>(message, ...others)=>{
+const $53382f7b3a533fc6$export$2b1d391cd05230fd = (fileName)=>(message, ...others)=>{
         console.log(`[${fileName}.js] ${message}`, ...others);
     }
 ;
-const $1feaf5137e56a072$export$b522027998979def = (fileName)=>(message, ...others)=>console.error(`[${fileName}.js] ${message}`, ...others)
+const $53382f7b3a533fc6$export$b522027998979def = (fileName)=>(message, ...others)=>console.error(`[${fileName}.js] ${message}`, ...others)
 ;
 
 
-const $07c03eb40a016611$var$log = $1feaf5137e56a072$export$2b1d391cd05230fd('background');
+const $76daf2678a5c69cb$var$log = $53382f7b3a533fc6$export$2b1d391cd05230fd('background');
 browser.runtime.onMessage.addListener(async (request, sender)=>{
-    $07c03eb40a016611$var$log('Receive message', request, request.getValue, sender);
+    $76daf2678a5c69cb$var$log('Receive message', request, request.getValue, sender);
     const message = request;
-    if (!message || message.destination !== $f047d79a9426f7d2$export$908ff786c04d5297.background) return undefined;
+    if (!message || message.destination !== $479e784236c1fcf9$export$908ff786c04d5297.background) return undefined;
     const { method: method , id: id  } = message.request;
     switch(method){
-        case $ebac0c3c7faf13a9$export$a7e1148f0e98483a.ConnectDapp:
+        case $49c5a02eaa106cab$export$a7e1148f0e98483a.ConnectDapp:
             {
-                $07c03eb40a016611$var$log('receive ETH request accounts', message.request);
-                const result = await $f047d79a9426f7d2$export$61027ddcc22c3df2(message.request);
-                $07c03eb40a016611$var$log('request accounts result', result);
+                $76daf2678a5c69cb$var$log('receive ETH request accounts', message.request);
+                const result = await $479e784236c1fcf9$export$61027ddcc22c3df2(message.request);
+                $76daf2678a5c69cb$var$log('request accounts result', result);
                 // await new Promise((resolve) => {
                 //   setTimeout(resolve, 3000);
                 // });
                 // const result = { id, value: ['0x51a4f8419aC902006211786a5648F0cc14aa7074'] };
                 return result;
             }
-        case $ebac0c3c7faf13a9$export$a7e1148f0e98483a.SignAndSendTransaction:
+        case $49c5a02eaa106cab$export$a7e1148f0e98483a.SignAndSendTransaction:
             {
-                $07c03eb40a016611$var$log('receive ETH send transaction', message.request);
-                const result = await $f047d79a9426f7d2$export$61027ddcc22c3df2(message.request);
-                $07c03eb40a016611$var$log('send transaction result', result);
+                $76daf2678a5c69cb$var$log('receive ETH send transaction', message.request);
+                const result = await $479e784236c1fcf9$export$61027ddcc22c3df2(message.request);
+                $76daf2678a5c69cb$var$log('send transaction result', result);
                 // await new Promise((resolve) => {
                 //   setTimeout(resolve, 4000);
                 // });
@@ -182,11 +182,11 @@ browser.runtime.onMessage.addListener(async (request, sender)=>{
                 // };
                 return result;
             }
-        case $ebac0c3c7faf13a9$export$a7e1148f0e98483a.SignPersonalMessage:
+        case $49c5a02eaa106cab$export$a7e1148f0e98483a.SignPersonalMessage:
             {
-                $07c03eb40a016611$var$log('receive ETH sign personal message', message.request);
-                const result = await $f047d79a9426f7d2$export$61027ddcc22c3df2(message.request);
-                $07c03eb40a016611$var$log('send transaction result', result);
+                $76daf2678a5c69cb$var$log('receive ETH sign personal message', message.request);
+                const result = await $479e784236c1fcf9$export$61027ddcc22c3df2(message.request);
+                $76daf2678a5c69cb$var$log('send transaction result', result);
                 // await new Promise((resolve) => {
                 //   setTimeout(resolve, 4000);
                 // });
@@ -196,24 +196,24 @@ browser.runtime.onMessage.addListener(async (request, sender)=>{
                 // };
                 return result;
             }
-        case $ebac0c3c7faf13a9$export$6f8959d005e6c8f0.ConnectDapp:
+        case $49c5a02eaa106cab$export$6f8959d005e6c8f0.ConnectDapp:
             {
-                $07c03eb40a016611$var$log('receive SOL request accounts', message.request);
-                const result = await $f047d79a9426f7d2$export$61027ddcc22c3df2(message.request);
-                $07c03eb40a016611$var$log('request accounts result', result);
+                $76daf2678a5c69cb$var$log('receive SOL request accounts', message.request);
+                const result = await $479e784236c1fcf9$export$61027ddcc22c3df2(message.request);
+                $76daf2678a5c69cb$var$log('request accounts result', result);
                 // await new Promise((resolve) => {
                 //   setTimeout(resolve, 3000);
                 // });
                 // const result = { id, value: ['GxhLFJ7XoCoxLdcLBzvycknww3yqzBfDe5ra61EdUEAR'] };
                 return result;
             }
-        case $ebac0c3c7faf13a9$export$6f8959d005e6c8f0.SignAndSendTransaction:
-        case $ebac0c3c7faf13a9$export$6f8959d005e6c8f0.SignTransaction:
-        case $ebac0c3c7faf13a9$export$6f8959d005e6c8f0.SignAllTransactions:
+        case $49c5a02eaa106cab$export$6f8959d005e6c8f0.SignAndSendTransaction:
+        case $49c5a02eaa106cab$export$6f8959d005e6c8f0.SignTransaction:
+        case $49c5a02eaa106cab$export$6f8959d005e6c8f0.SignAllTransactions:
             {
-                $07c03eb40a016611$var$log('receive SOL sign and send transaction', message.request);
-                const result = await $f047d79a9426f7d2$export$61027ddcc22c3df2(message.request);
-                $07c03eb40a016611$var$log('solana sign transaction result', result);
+                $76daf2678a5c69cb$var$log('receive SOL sign and send transaction', message.request);
+                const result = await $479e784236c1fcf9$export$61027ddcc22c3df2(message.request);
+                $76daf2678a5c69cb$var$log('solana sign transaction result', result);
                 // await new Promise((resolve) => {
                 //   setTimeout(resolve, 3000);
                 // });
@@ -224,11 +224,11 @@ browser.runtime.onMessage.addListener(async (request, sender)=>{
                 return result;
             }
         default:
-            $07c03eb40a016611$var$log(`unexpected request method ${method}`);
+            $76daf2678a5c69cb$var$log(`unexpected request method ${method}`);
     }
     return undefined;
 });
-$07c03eb40a016611$var$log(`loaded`);
+$76daf2678a5c69cb$var$log(`loaded`);
 
 })();
 //# sourceMappingURL=background.js.map
